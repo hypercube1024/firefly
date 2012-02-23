0c5e0e717f1d9bf72297fc68708b89d00d92049c
hypercube1024 <qptkk@163.com>
2012-02-24 00:21:31 +0800

    up

--- firefly/src/test/java/test/http/ServerDemo.java
+++ firefly/src/test/java/test/http/ServerDemo.java
@@ -7,8 +7,7 @@ import com.firefly.server.ServerBootstrap;
 public class ServerDemo {
 
 	public static void main(String[] args) throws Throwable {
-		String serverHome = new File(ServerBootstrap.class.getResource("/page")
-				.toURI()).getAbsolutePath();
+		String serverHome = new File(ServerBootstrap.class.getResource("/page").toURI()).getAbsolutePath();
 		ServerBootstrap.start("firefly-server.xml", serverHome, "localhost", 6655);
 	}
 
