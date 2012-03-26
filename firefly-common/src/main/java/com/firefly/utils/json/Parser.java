eb6ee264a7b8cbff00727dd51bdf296929f3890a
hypercube1024 <qptkk@163.com>
2012-03-27 00:07:34 +0800

    增加parser方法

--- firefly-common/src/main/java/com/firefly/utils/json/Parser.java
+++ firefly-common/src/main/java/com/firefly/utils/json/Parser.java
@@ -3,5 +3,7 @@ package com.firefly.utils.json;
 import com.firefly.utils.json.support.JsonStringReader;
 
 public interface Parser {
-	void convertTo(JsonStringReader reader);
+	void convertToObj(JsonStringReader reader, Object obj);
+	
+	Object convertToSimple(JsonStringReader reader);
 }
