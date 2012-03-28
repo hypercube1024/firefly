53eef6b8fc756cce84e68db3957b40de63953f4f
hypercube1024 <qptkk@163.com>
2012-03-28 12:31:36 +0800

    增加测试页面

--- firefly/src/test/java/test/http/ServerDebug.java
+++ firefly/src/test/java/test/http/ServerDebug.java
@@ -7,17 +7,21 @@ import java.net.Socket;
 public class ServerDebug {
 
 	public static void main(String[] args) throws Throwable{
-//		String msg =
-//				"GET /index.html HTTP/1.1\r\n" +
-//				"Host: 127.0.0.1\r\n" +
-//				"Range: bytes=3-10,-7, 20-\r\n\r\n";
+		String msg =
+				"GET /test/index.html HTTP/1.1\r\n" +
+				"Host: 127.0.0.1\r\n" +
+				"Range: bytes=3-10,-7\r\n\r\n";
 //		String msg = 
 //				"GET /app/index HTTP/1.1\r\n" +
 //				"Host: 127.0.0.1\r\n\r\n";
-		String msg = 
-				"POST /index.html HTTP/1.1\r\n" +
-				"Host: 127.0.0.1\r\n" +
-				"Expect: 100-continue\r\n\r\n";
+//		String msg = 
+//				"POST /index.html HTTP/1.1\r\n" +
+//				"Host: 127.0.0.1\r\n" +
+//				"Expect: 100-continue\r\n\r\n";
+		test(msg);
+	}
+	
+	public static void test(String msg) throws Throwable{
 		Socket socket = new Socket("localhost", 6655);
 		OutputStream out = socket.getOutputStream();
 		out.write(msg.getBytes("UTF-8"));
