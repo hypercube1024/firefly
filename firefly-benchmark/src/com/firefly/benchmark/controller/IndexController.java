b07068d8eb0694183de5af15ffd948b63ca33214
hypercube1024 <qptkk@163.com>
2012-10-24 15:18:05 +0800

    增加性能测试用例

--- firefly-benchmark/src/com/firefly/benchmark/controller/IndexController.java
+++ firefly-benchmark/src/com/firefly/benchmark/controller/IndexController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.firefly.annotation.Controller;
+import com.firefly.annotation.PathVariable;
 import com.firefly.annotation.RequestMapping;
 import com.firefly.mvc.web.View;
 import com.firefly.mvc.web.view.TemplateView;
@@ -12,7 +13,13 @@ import com.firefly.mvc.web.view.TemplateView;
 public class IndexController {
 	@RequestMapping(value = "/index")
 	public View index(HttpServletRequest request, HttpServletResponse response) {
-		request.setAttribute("hello", "hello firefly");
+		request.setAttribute("info", new String[]{"hello firefly", "test"});
+		return new TemplateView("/index.html");
+	}
+	
+	@RequestMapping(value = "/document/?/?")
+	public View document(HttpServletRequest request, @PathVariable String[] args) {
+		request.setAttribute("info", args);
 		return new TemplateView("/index.html");
 	}
 }
