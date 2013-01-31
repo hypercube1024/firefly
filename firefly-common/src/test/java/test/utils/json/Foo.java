e878f6b31ca79cf48e9c6ea9d349c8438b2ef783
hypercube1024 <qptkk@163.com>
2013-01-31 09:51:37 +0800

    修正首次序列化，造型阶段时，读取父类public get方法的私有字段打印异常信息

---
+++ firefly-common/src/test/java/test/utils/json/Foo.java
@@ -0,0 +1,23 @@
+package test.utils.json;
+
+public class Foo {
+	private String text, foo;
+
+	public String getFoo() {
+		return foo;
+	}
+
+	public void setFoo(String foo) {
+		this.foo = foo;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+}
