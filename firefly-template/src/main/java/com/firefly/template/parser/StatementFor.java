d0c9bf7c5130fae65e715af8d4e6e262b378b29e
hypercube1024 <qptkk@163.com>
2013-03-07 13:07:46 +0800

    修正for自增变量冲突问题

--- firefly-template/src/main/java/com/firefly/template/parser/StatementFor.java
+++ firefly-template/src/main/java/com/firefly/template/parser/StatementFor.java
@@ -1,16 +1,26 @@
 package com.firefly.template.parser;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.firefly.utils.StringUtils;
 
 public class StatementFor implements Statement {
 
+	private static final Set<String> vari = new HashSet<String>();
+	
 	@Override
 	public void parse(String content, JavaFileBuilder javaFileBuilder) {
 		String[] e = StringUtils.split(content, ':');
 		e[0] = e[0].trim();
 		e[1] = e[1].trim();
 
-		javaFileBuilder.write(javaFileBuilder.getPreBlank() + "int " + e[0] + "_index = -1;\n");
+		if(vari.contains(e[0])) {
+			javaFileBuilder.write(javaFileBuilder.getPreBlank() + e[0] + "_index = -1;\n");
+		} else {
+			javaFileBuilder.write(javaFileBuilder.getPreBlank() + "int " + e[0] + "_index = -1;\n");
+			vari.add(e[0]);
+		}
 		javaFileBuilder.write(javaFileBuilder.getPreBlank() + "for(Object "
 				+ e[0] + " : objNav.getCollection(model, \""
 				+ e[1].substring(e[1].indexOf("${") + 2, e[1].length() - 1)
