3c4c6d70e48ba1be35027a4f388bf22ae426cb2d
hypercube1024 <qptkk@163.com>
2013-03-14 18:21:43 +0800

    删除循环自增变量

--- firefly-template/src/main/java/com/firefly/template/parser/StatementFor.java
+++ firefly-template/src/main/java/com/firefly/template/parser/StatementFor.java
@@ -1,13 +1,8 @@
 package com.firefly.template.parser;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import com.firefly.utils.StringUtils;
 
 public class StatementFor implements Statement {
-
-	private static final Set<String> vari = new HashSet<String>();
 	
 	@Override
 	public void parse(String content, JavaFileBuilder javaFileBuilder) {
@@ -15,23 +10,12 @@ public class StatementFor implements Statement {
 		e[0] = e[0].trim();
 		e[1] = e[1].trim();
 
-		String k = javaFileBuilder.getName() + "|" + e[0];
-		if(vari.contains(k)) {
-			javaFileBuilder.write(javaFileBuilder.getPreBlank() + e[0] + "_index = -1;\n");
-		} else {
-			javaFileBuilder.write(javaFileBuilder.getPreBlank() + "int " + e[0] + "_index = -1;\n");
-			vari.add(k);
-		}
 		javaFileBuilder.write(javaFileBuilder.getPreBlank() + "for(Object "
 				+ e[0] + " : objNav.getCollection(model, \""
 				+ e[1].substring(e[1].indexOf("${") + 2, e[1].length() - 1)
 				+ "\")){\n");
 		javaFileBuilder.getPreBlank().append('\t');
-		javaFileBuilder.write(javaFileBuilder.getPreBlank() + e[0] + "_index++;\n");
-		javaFileBuilder.write(javaFileBuilder.getPreBlank() + "model.put(\""
-				+ e[0] + "\", " + e[0] + ");\n");
-		javaFileBuilder.write(javaFileBuilder.getPreBlank() + "model.put(\""
-				+ e[0] + "_index\", " + e[0] + "_index);\n");
+		javaFileBuilder.write(javaFileBuilder.getPreBlank() + "model.put(\"" + e[0] + "\", " + e[0] + ");\n");
 	}
 
 }
