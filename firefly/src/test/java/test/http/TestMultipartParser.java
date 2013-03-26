933099bcc90c4deffc87bd703433e608d23f69af
hypercube1024 <qptkk@163.com>
2013-03-26 22:59:55 +0800

    增加MultipartParser自动测试用例

--- firefly/src/test/java/test/http/TestMultipartParser.java
+++ firefly/src/test/java/test/http/TestMultipartParser.java
@@ -24,38 +24,49 @@ public class TestMultipartParser {
 	@Test
 	public void testMultipartParser() throws Throwable {
 		PartImpl.tempdir = new File(ServerBootstrap.class.getResource("/page/template/_firefly_tmpdir").toURI()).getAbsolutePath();
-		File testFile = new File(ServerBootstrap.class.getResource("/83a6ad14-b318-4ffd-857c-d476129f3fb1").toURI());
+		File testFile = new File(ServerBootstrap.class.getResource("/38d36404-53a5-456b-9f63-1e127bf86975").toURI());
 		
 		final BufferedInputStream in = new BufferedInputStream(new FileInputStream(testFile));
-		Collection<Part> col = MultipartFormDataParser.parse(new ServletInputStream() {
+		Collection<Part> col = null;
+		try {
+			col = MultipartFormDataParser.parse(new ServletInputStream() {
 			
-			@Override
-			public int read() throws IOException {
-				return in.read();
-			}
-			
-			@Override
-			public int available() throws IOException {
-				return in.available();
-			}
-
-			@Override
-			public void close() throws IOException {
-				in.close();
-			}
-
-			public int read(byte[] b, int off, int len) throws IOException {
-				return in.read(b, off, len);
-			}
-		}, "multipart/form-data; boundary=----WebKitFormBoundarysQJ5u5rwAAwNHszq", "UTF-8");
-		
+				@Override
+				public int read() throws IOException {
+					return in.read();
+				}
+				
+				@Override
+				public int available() throws IOException {
+					return in.available();
+				}
+	
+				@Override
+				public void close() throws IOException {
+					in.close();
+				}
+	
+				public int read(byte[] b, int off, int len) throws IOException {
+					return in.read(b, off, len);
+				}
+			}, "multipart/form-data; boundary=----WebKitFormBoundarytqSp9IL39DiccYay", "UTF-8");
+		} finally {
+			in.close();
+		}
 		
 		MultipartFormData m = new MultipartFormData(col);
-		Assert.assertThat(col.size(), is(4));
-		Assert.assertThat(m.getPart("price").getName(), is("price"));
-		Assert.assertThat(m.getPart("price").getSize(), is(4L));
-
-		in.close();
-		m.close();
+		try {
+			Assert.assertThat(col.size(), is(4));
+			Assert.assertThat(m.getPart("price").getName(), is("price"));
+			Assert.assertThat(m.getPart("price").getSize(), is(4L));
+			Assert.assertThat(m.getPart("content1").getSize(), is(3701282L));
+			Assert.assertThat(m.getPart("content2").getSize(), is(5760061L));
+		} finally {
+			m.close();
+		}
 	}
+	
+//	public static void main(String[] args) throws Throwable {
+//		new TestMultipartParser().testMultipartParser();
+//	}
 }
