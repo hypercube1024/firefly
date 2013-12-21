92b27589b0bae3c0365ddc1cf6f943cc168cf0a3
hypercube1024 <qptkk@163.com>
2013-12-21 16:57:12 +0800

    modify test caase

--- firefly/src/test/java/test/http/ServerDemo.java
+++ firefly/src/test/java/test/http/ServerDemo.java
@@ -1,10 +1,6 @@
 package test.http;
 
-import java.io.File;
-
-import com.firefly.mvc.web.servlet.SystemHtmlPage;
 import com.firefly.server.ServerBootstrap;
-import com.firefly.server.http.Config;
 
 public class ServerDemo {
 
@@ -12,25 +8,8 @@ public class ServerDemo {
 		start1();
 	}
 	
-	public static void start1() throws Throwable {
-		SystemHtmlPage.addErrorPage(404, "/error/err404.html");
-		SystemHtmlPage.addErrorPage(500, "/error/err500.html");
-		String serverHome = new File(ServerBootstrap.class.getResource("/page").toURI()).getAbsolutePath();
-		ServerBootstrap.start("firefly-server.xml", serverHome, "localhost", 6655);
-	}
-	
-	public static void start2() {
+	public static void start1() {
 		ServerBootstrap.start("firefly-server.xml");
-	}
-	
-	public static void start3() throws Throwable {
-		String serverHome = new File(ServerBootstrap.class.getResource("/page").toURI()).getAbsolutePath();
-		Config config = new Config();
-		config.setHost("localhost");
-		config.setPort(8080);
-		config.setServerHome(serverHome);
-		config.setPipeline(false);
-		ServerBootstrap.start(config);
 	}
 
 }
