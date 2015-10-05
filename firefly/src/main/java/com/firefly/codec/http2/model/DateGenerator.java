90f0c4fd07f303ce04ffa3ae37e865e1a8c3a6c5
hypercube1024 <qptkk@163.com>
2015-10-06 00:20:27 +0800

    add HTTP generator

--- firefly/src/main/java/com/firefly/codec/http2/model/DateGenerator.java
+++ firefly/src/main/java/com/firefly/codec/http2/model/DateGenerator.java
@@ -6,151 +6,151 @@ import java.util.TimeZone;
 
 import com.firefly.utils.StringUtils;
 
-
 /**
  * ThreadLocal Date formatters for HTTP style dates.
  */
-public class DateGenerator
-{
-    private static final TimeZone __GMT = TimeZone.getTimeZone("GMT");
-    static
-    {
-        __GMT.setID("GMT");
-    }
-    
-    static final String[] DAYS =
-        { "Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-    static final String[] MONTHS =
-        { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Jan"};
+public class DateGenerator {
+	private static final TimeZone __GMT = TimeZone.getTimeZone("GMT");
 
+	static {
+		__GMT.setID("GMT");
+	}
 
-    private static final ThreadLocal<DateGenerator> __dateGenerator =new ThreadLocal<DateGenerator>()
-    {
-        @Override
-        protected DateGenerator initialValue()
-        {
-            return new DateGenerator();
-        }
-    };
+	static final String[] DAYS = { "Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+	static final String[] MONTHS = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+			"Jan" };
 
+	private static final ThreadLocal<DateGenerator> __dateGenerator = new ThreadLocal<DateGenerator>() {
+		@Override
+		protected DateGenerator initialValue() {
+			return new DateGenerator();
+		}
+	};
 
-    public final static String __01Jan1970=DateGenerator.formatDate(0);
-    
-    /**
-     * Format HTTP date "EEE, dd MMM yyyy HH:mm:ss 'GMT'"
-     * @param date the date in milliseconds
-     * @return the formatted date
-     */
-    public static String formatDate(long date)
-    {
-        return __dateGenerator.get().doFormatDate(date);
-    }
+	public final static String __01Jan1970 = DateGenerator.formatDate(0);
 
-    /**
-     * Format "EEE, dd-MMM-yyyy HH:mm:ss 'GMT'" for cookies
-     * @param buf the buffer to put the formatted date into
-     * @param date the date in milliseconds
-     */
-    public static void formatCookieDate(StringBuilder buf, long date)
-    {
-        __dateGenerator.get().doFormatCookieDate(buf,date);
-    }
+	/**
+	 * Format HTTP date "EEE, dd MMM yyyy HH:mm:ss 'GMT'"
+	 * 
+	 * @param date
+	 *            the date in milliseconds
+	 * @return the formatted date
+	 */
+	public static String formatDate(long date) {
+		return __dateGenerator.get().doFormatDate(date);
+	}
 
-    /**
-     * Format "EEE, dd-MMM-yyyy HH:mm:ss 'GMT'" for cookies
-     * @param date the date in milliseconds 
-     * @return the formatted date
-     */
-    public static String formatCookieDate(long date)
-    {
-        StringBuilder buf = new StringBuilder(28);
-        formatCookieDate(buf, date);
-        return buf.toString();
-    }
-    
-    private final StringBuilder buf = new StringBuilder(32);
-    private final GregorianCalendar gc = new GregorianCalendar(__GMT);
+	/**
+	 * Format "EEE, dd-MMM-yyyy HH:mm:ss 'GMT'" for cookies
+	 * 
+	 * @param buf
+	 *            the buffer to put the formatted date into
+	 * @param date
+	 *            the date in milliseconds
+	 */
+	public static void formatCookieDate(StringBuilder buf, long date) {
+		__dateGenerator.get().doFormatCookieDate(buf, date);
+	}
 
-    /**
-     * Format HTTP date "EEE, dd MMM yyyy HH:mm:ss 'GMT'"
-     * @param date the date in milliseconds
-     * @return the formatted date
-     */
-    public String doFormatDate(long date)
-    {
-        buf.setLength(0);
-        gc.setTimeInMillis(date);
+	/**
+	 * Format "EEE, dd-MMM-yyyy HH:mm:ss 'GMT'" for cookies
+	 * 
+	 * @param date
+	 *            the date in milliseconds
+	 * @return the formatted date
+	 */
+	public static String formatCookieDate(long date) {
+		StringBuilder buf = new StringBuilder(28);
+		formatCookieDate(buf, date);
+		return buf.toString();
+	}
 
-        int day_of_week = gc.get(Calendar.DAY_OF_WEEK);
-        int day_of_month = gc.get(Calendar.DAY_OF_MONTH);
-        int month = gc.get(Calendar.MONTH);
-        int year = gc.get(Calendar.YEAR);
-        int century = year / 100;
-        year = year % 100;
+	private final StringBuilder buf = new StringBuilder(32);
+	private final GregorianCalendar gc = new GregorianCalendar(__GMT);
 
-        int hours = gc.get(Calendar.HOUR_OF_DAY);
-        int minutes = gc.get(Calendar.MINUTE);
-        int seconds = gc.get(Calendar.SECOND);
+	/**
+	 * Format HTTP date "EEE, dd MMM yyyy HH:mm:ss 'GMT'"
+	 * 
+	 * @param date
+	 *            the date in milliseconds
+	 * @return the formatted date
+	 */
+	public String doFormatDate(long date) {
+		buf.setLength(0);
+		gc.setTimeInMillis(date);
 
-        buf.append(DAYS[day_of_week]);
-        buf.append(',');
-        buf.append(' ');
-        StringUtils.append2digits(buf, day_of_month);
+		int day_of_week = gc.get(Calendar.DAY_OF_WEEK);
+		int day_of_month = gc.get(Calendar.DAY_OF_MONTH);
+		int month = gc.get(Calendar.MONTH);
+		int year = gc.get(Calendar.YEAR);
+		int century = year / 100;
+		year = year % 100;
 
-        buf.append(' ');
-        buf.append(MONTHS[month]);
-        buf.append(' ');
-        StringUtils.append2digits(buf, century);
-        StringUtils.append2digits(buf, year);
+		int hours = gc.get(Calendar.HOUR_OF_DAY);
+		int minutes = gc.get(Calendar.MINUTE);
+		int seconds = gc.get(Calendar.SECOND);
 
-        buf.append(' ');
-        StringUtils.append2digits(buf, hours);
-        buf.append(':');
-        StringUtils.append2digits(buf, minutes);
-        buf.append(':');
-        StringUtils.append2digits(buf, seconds);
-        buf.append(" GMT");
-        return buf.toString();
-    }
+		buf.append(DAYS[day_of_week]);
+		buf.append(',');
+		buf.append(' ');
+		StringUtils.append2digits(buf, day_of_month);
 
-    /**
-     * Format "EEE, dd-MMM-yy HH:mm:ss 'GMT'" for cookies
-     * @param buf the buffer to format the date into
-     * @param date the date in milliseconds
-     */
-    public void doFormatCookieDate(StringBuilder buf, long date)
-    {
-        gc.setTimeInMillis(date);
+		buf.append(' ');
+		buf.append(MONTHS[month]);
+		buf.append(' ');
+		StringUtils.append2digits(buf, century);
+		StringUtils.append2digits(buf, year);
 
-        int day_of_week = gc.get(Calendar.DAY_OF_WEEK);
-        int day_of_month = gc.get(Calendar.DAY_OF_MONTH);
-        int month = gc.get(Calendar.MONTH);
-        int year = gc.get(Calendar.YEAR);
-        year = year % 10000;
+		buf.append(' ');
+		StringUtils.append2digits(buf, hours);
+		buf.append(':');
+		StringUtils.append2digits(buf, minutes);
+		buf.append(':');
+		StringUtils.append2digits(buf, seconds);
+		buf.append(" GMT");
+		return buf.toString();
+	}
 
-        int epoch = (int) ((date / 1000) % (60 * 60 * 24));
-        int seconds = epoch % 60;
-        epoch = epoch / 60;
-        int minutes = epoch % 60;
-        int hours = epoch / 60;
+	/**
+	 * Format "EEE, dd-MMM-yy HH:mm:ss 'GMT'" for cookies
+	 * 
+	 * @param buf
+	 *            the buffer to format the date into
+	 * @param date
+	 *            the date in milliseconds
+	 */
+	public void doFormatCookieDate(StringBuilder buf, long date) {
+		gc.setTimeInMillis(date);
 
-        buf.append(DAYS[day_of_week]);
-        buf.append(',');
-        buf.append(' ');
-        StringUtils.append2digits(buf, day_of_month);
+		int day_of_week = gc.get(Calendar.DAY_OF_WEEK);
+		int day_of_month = gc.get(Calendar.DAY_OF_MONTH);
+		int month = gc.get(Calendar.MONTH);
+		int year = gc.get(Calendar.YEAR);
+		year = year % 10000;
 
-        buf.append('-');
-        buf.append(MONTHS[month]);
-        buf.append('-');
-        StringUtils.append2digits(buf, year/100);
-        StringUtils.append2digits(buf, year%100);
+		int epoch = (int) ((date / 1000) % (60 * 60 * 24));
+		int seconds = epoch % 60;
+		epoch = epoch / 60;
+		int minutes = epoch % 60;
+		int hours = epoch / 60;
 
-        buf.append(' ');
-        StringUtils.append2digits(buf, hours);
-        buf.append(':');
-        StringUtils.append2digits(buf, minutes);
-        buf.append(':');
-        StringUtils.append2digits(buf, seconds);
-        buf.append(" GMT");
-    }
+		buf.append(DAYS[day_of_week]);
+		buf.append(',');
+		buf.append(' ');
+		StringUtils.append2digits(buf, day_of_month);
+
+		buf.append('-');
+		buf.append(MONTHS[month]);
+		buf.append('-');
+		StringUtils.append2digits(buf, year / 100);
+		StringUtils.append2digits(buf, year % 100);
+
+		buf.append(' ');
+		StringUtils.append2digits(buf, hours);
+		buf.append(':');
+		StringUtils.append2digits(buf, minutes);
+		buf.append(':');
+		StringUtils.append2digits(buf, seconds);
+		buf.append(" GMT");
+	}
 }
--- firefly/src/main/java/com/firefly/codec/http2/model/DateParser.java
+++ firefly/src/main/java/com/firefly/codec/http2/model/DateParser.java
@@ -9,83 +9,63 @@ import java.util.TimeZone;
  * ThreadLocal data parsers for HTTP style dates
  *
  */
-public class DateParser
-{
-    private static final TimeZone __GMT = TimeZone.getTimeZone("GMT");
-    static
-    {
-        __GMT.setID("GMT");
-    }
-    
-    final static String __dateReceiveFmt[] =
-    {
-        "EEE, dd MMM yyyy HH:mm:ss zzz",
-        "EEE, dd-MMM-yy HH:mm:ss",
-        "EEE MMM dd HH:mm:ss yyyy",
+public class DateParser {
+	private static final TimeZone __GMT = TimeZone.getTimeZone("GMT");
 
-        "EEE, dd MMM yyyy HH:mm:ss", "EEE dd MMM yyyy HH:mm:ss zzz",
-        "EEE dd MMM yyyy HH:mm:ss", "EEE MMM dd yyyy HH:mm:ss zzz", "EEE MMM dd yyyy HH:mm:ss",
-        "EEE MMM-dd-yyyy HH:mm:ss zzz", "EEE MMM-dd-yyyy HH:mm:ss", "dd MMM yyyy HH:mm:ss zzz",
-        "dd MMM yyyy HH:mm:ss", "dd-MMM-yy HH:mm:ss zzz", "dd-MMM-yy HH:mm:ss", "MMM dd HH:mm:ss yyyy zzz",
-        "MMM dd HH:mm:ss yyyy", "EEE MMM dd HH:mm:ss yyyy zzz",
-        "EEE, MMM dd HH:mm:ss yyyy zzz", "EEE, MMM dd HH:mm:ss yyyy", "EEE, dd-MMM-yy HH:mm:ss zzz",
-        "EEE dd-MMM-yy HH:mm:ss zzz", "EEE dd-MMM-yy HH:mm:ss",
-    };
+	static {
+		__GMT.setID("GMT");
+	}
 
-    public static long parseDate(String date)
-    {
-        return __dateParser.get().parse(date);
-    }
+	final static String __dateReceiveFmt[] = { "EEE, dd MMM yyyy HH:mm:ss zzz", "EEE, dd-MMM-yy HH:mm:ss",
+			"EEE MMM dd HH:mm:ss yyyy",
 
-    private static final ThreadLocal<DateParser> __dateParser =new ThreadLocal<DateParser>()
-    {
-        @Override
-        protected DateParser initialValue()
-        {
-            return new DateParser();
-        }
-    };
-    
-    final SimpleDateFormat _dateReceive[]= new SimpleDateFormat[__dateReceiveFmt.length];
+			"EEE, dd MMM yyyy HH:mm:ss", "EEE dd MMM yyyy HH:mm:ss zzz", "EEE dd MMM yyyy HH:mm:ss",
+			"EEE MMM dd yyyy HH:mm:ss zzz", "EEE MMM dd yyyy HH:mm:ss", "EEE MMM-dd-yyyy HH:mm:ss zzz",
+			"EEE MMM-dd-yyyy HH:mm:ss", "dd MMM yyyy HH:mm:ss zzz", "dd MMM yyyy HH:mm:ss", "dd-MMM-yy HH:mm:ss zzz",
+			"dd-MMM-yy HH:mm:ss", "MMM dd HH:mm:ss yyyy zzz", "MMM dd HH:mm:ss yyyy", "EEE MMM dd HH:mm:ss yyyy zzz",
+			"EEE, MMM dd HH:mm:ss yyyy zzz", "EEE, MMM dd HH:mm:ss yyyy", "EEE, dd-MMM-yy HH:mm:ss zzz",
+			"EEE dd-MMM-yy HH:mm:ss zzz", "EEE dd-MMM-yy HH:mm:ss", };
 
-    private long parse(final String dateVal)
-    {
-        for (int i = 0; i < _dateReceive.length; i++)
-        {
-            if (_dateReceive[i] == null)
-            {
-                _dateReceive[i] = new SimpleDateFormat(__dateReceiveFmt[i], Locale.US);
-                _dateReceive[i].setTimeZone(__GMT);
-            }
+	public static long parseDate(String date) {
+		return __dateParser.get().parse(date);
+	}
 
-            try
-            {
-                Date date = (Date) _dateReceive[i].parseObject(dateVal);
-                return date.getTime();
-            }
-            catch (java.lang.Exception e)
-            {
-                // LOG.ignore(e);
-            }
-        }
+	private static final ThreadLocal<DateParser> __dateParser = new ThreadLocal<DateParser>() {
+		@Override
+		protected DateParser initialValue() {
+			return new DateParser();
+		}
+	};
 
-        if (dateVal.endsWith(" GMT"))
-        {
-            final String val = dateVal.substring(0, dateVal.length() - 4);
+	final SimpleDateFormat _dateReceive[] = new SimpleDateFormat[__dateReceiveFmt.length];
 
-            for (SimpleDateFormat element : _dateReceive)
-            {
-                try
-                {
-                    Date date = (Date) element.parseObject(val);
-                    return date.getTime();
-                }
-                catch (java.lang.Exception e)
-                {
-                    // LOG.ignore(e);
-                }
-            }
-        }
-        return -1;
-    }
+	private long parse(final String dateVal) {
+		for (int i = 0; i < _dateReceive.length; i++) {
+			if (_dateReceive[i] == null) {
+				_dateReceive[i] = new SimpleDateFormat(__dateReceiveFmt[i], Locale.US);
+				_dateReceive[i].setTimeZone(__GMT);
+			}
+
+			try {
+				Date date = (Date) _dateReceive[i].parseObject(dateVal);
+				return date.getTime();
+			} catch (java.lang.Exception e) {
+				// LOG.ignore(e);
+			}
+		}
+
+		if (dateVal.endsWith(" GMT")) {
+			final String val = dateVal.substring(0, dateVal.length() - 4);
+
+			for (SimpleDateFormat element : _dateReceive) {
+				try {
+					Date date = (Date) element.parseObject(val);
+					return date.getTime();
+				} catch (java.lang.Exception e) {
+					// LOG.ignore(e);
+				}
+			}
+		}
+		return -1;
+	}
 }
