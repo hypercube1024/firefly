a3d065c3cf1d78f6f91b6f90347fa7322f0a1822
PengPeng <qptkk@163.com>
2016-01-29 16:36:12 +0800

    add SoftReferenceConcurrentHashMap and WeakReferenceConcurrentHashMap

---
+++ firefly-common/src/main/java/com/firefly/utils/collection/WeakReferenceConcurrentHashMap.java
@@ -0,0 +1,30 @@
+package com.firefly.utils.collection;
+
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+
+public class WeakReferenceConcurrentHashMap<K, V> extends AbstractReferenceConcurrentHashMap<K, V> {
+
+	private class ValueWeakReference extends WeakReference<V> {
+
+		K key;
+
+		public ValueWeakReference(K key, V value) {
+			super(value, refQueue);
+			this.key = key;
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void clearInvalidEntry(Reference<? extends V> ref) {
+		map.remove(((ValueWeakReference) ref).key);
+	}
+
+	@Override
+	protected Reference<V> createRefence(K key, V value) {
+		return new ValueWeakReference(key, value);
+	}
+
+}
---
+++ firefly-common/src/test/java/test/utils/collection/TestWeakReferenceConcurrentHashMap.java
@@ -0,0 +1,32 @@
+package test.utils.collection;
+
+import java.util.UUID;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.firefly.utils.collection.AbstractReferenceConcurrentHashMap;
+import com.firefly.utils.collection.WeakReferenceConcurrentHashMap;
+
+public class TestWeakReferenceConcurrentHashMap {
+
+	@Test
+	public void test() {
+		AbstractReferenceConcurrentHashMap<String, String> map = new WeakReferenceConcurrentHashMap<>();
+
+		long i = 0;
+		while (true) {
+			i = i + 1;
+			map.put("key" + i, UUID.randomUUID().toString());
+//			System.out.println(map.get("key1") + "|" + map.size());
+			if (map.get("key1") == null)
+				break;
+		}
+		System.out.println(i);
+		System.out.println(map.size());
+//		System.out.println(map.entrySet());
+//		System.out.println(map.size());
+		Assert.assertTrue(i >= map.size());
+	}
+
+}
