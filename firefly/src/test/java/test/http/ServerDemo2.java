d1c2d23eb59e0b3e48482c0878e395cde792f8f2
hypercube1024 <qptkk@163.com>
2016-07-29 10:31:12 +0800

    add comment

--- firefly/src/test/java/test/http/ServerDemo2.java
+++ firefly/src/test/java/test/http/ServerDemo2.java
@@ -4,6 +4,11 @@ import com.firefly.server.http2.servlet.ServerBootstrap;
 
 public class ServerDemo2 {
 
+	/**
+	 * Enable ALPN support -Xbootclasspath/p:[path_to_alpn_boot_jar]
+	 * @param args
+	 * @throws Throwable
+	 */
 	public static void main(String[] args) throws Throwable {
 //		System.setProperty("javax.net.debug", "all");
 		start();
