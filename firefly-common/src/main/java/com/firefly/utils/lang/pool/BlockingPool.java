b9b6c7ccd20b62c61b5781b9388e188ab2d7fe33
hypercube1024 <qptkk@163.com>
2016-08-31 11:51:31 +0800

    add object pool interface

--- firefly-common/src/main/java/com/firefly/utils/lang/pool/BlockingPool.java
+++ firefly-common/src/main/java/com/firefly/utils/lang/pool/BlockingPool.java
@@ -16,9 +16,9 @@ public interface BlockingPool<T> extends Pool<T> {
 	 *
 	 * Clients are advised to react to InterruptedException. If the thread is
 	 * interrupted while waiting for an object to become available, the current
-	 * implementations sets the interrupted state of the thread to
-	 * <code>true</code> and returns null. However this is subject to change
-	 * from implementation to implementation.
+	 * implementations sets the interrupted state of the thread to true and
+	 * returns null. However this is subject to change from implementation to
+	 * implementation.
 	 *
 	 * @return T an instance of the Object of type T from the pool.
 	 */
