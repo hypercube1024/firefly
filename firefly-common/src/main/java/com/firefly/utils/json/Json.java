8eb33524024f491046d81e089701ff4f4a03bf2b
hypercube1024 <qptkk@163.com>
2016-10-09 17:47:56 +0800

    clean up redundant finally code block

--- firefly-common/src/main/java/com/firefly/utils/json/Json.java
+++ firefly-common/src/main/java/com/firefly/utils/json/Json.java
@@ -8,83 +8,44 @@ import com.firefly.utils.json.parser.GeneralJSONObjectStateMacine;
 import com.firefly.utils.json.parser.ParserStateMachine;
 import com.firefly.utils.json.serializer.SerialStateMachine;
 
-
 public abstract class Json {
+
 	public static String toJson(Object obj) {
-		String ret = null;
-		JsonWriter writer = null;
-		try {
-			writer = new JsonStringWriter();
+		try (JsonWriter writer = new JsonStringWriter()) {
 			SerialStateMachine.toJson(obj, writer);
-			ret = writer.toString();
+			return writer.toString();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			if(writer != null)
-				try {
-					writer.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+			return null;
 		}
-		return ret;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static <T> T toObject(String json, Class<T> clazz) {
-		JsonReader reader = null;
-		T ret = null;
-		try {
-			reader = new JsonStringReader(json);
-			ret = (T) ParserStateMachine.toObject(reader, clazz);
+		try (JsonReader reader = new JsonStringReader(json)) {
+			return (T) ParserStateMachine.toObject(reader, clazz);
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			if(reader != null)
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+			return null;
 		}
-		return ret;
 	}
-	
+
 	public static JsonObject toJsonObject(String json) {
-		JsonReader reader = null;
-		JsonObject ret = null;
-		try {
-			reader = new JsonStringReader(json);
-			ret = GeneralJSONObjectStateMacine.toJsonObject(reader);
+		try (JsonReader reader = new JsonStringReader(json)) {
+			return GeneralJSONObjectStateMacine.toJsonObject(reader);
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			if(reader != null)
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+			return null;
 		}
-		return ret;
 	}
-	
+
 	public static JsonArray toJsonArray(String json) {
-		JsonReader reader = null;
-		JsonArray ret = null;
-		try {
-			reader = new JsonStringReader(json);
-			ret = GeneralJSONObjectStateMacine.toJsonArray(reader);
+		try (JsonReader reader = new JsonStringReader(json)) {
+			return GeneralJSONObjectStateMacine.toJsonArray(reader);
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			if(reader != null)
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+			return null;
 		}
-		return ret;
 	}
+
 }
