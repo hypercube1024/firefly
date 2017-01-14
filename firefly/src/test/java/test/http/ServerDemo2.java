a22c3535de3b7889a391c46ebef122a359bdf536
hypercube1024 <qptkk@163.com>
2017-01-14 13:44:09 +0800

    remove idk ssl support

--- firefly/src/test/java/test/http/ServerDemo2.java
+++ firefly/src/test/java/test/http/ServerDemo2.java
@@ -4,30 +4,13 @@ import com.firefly.server.http2.servlet.ServerBootstrap;
 
 public class ServerDemo2 {
 
-	/**
-	 * Enable ALPN support -Xbootclasspath/p:[path_to_alpn_boot_jar]
-	 * @param args
-	 * @throws Throwable
-	 */
-	public static void main(String[] args) throws Throwable {
-//		System.setProperty("javax.net.debug", "all");
-		start();
-	}
 
-	/**
-	 * Generate SSL credentials:
-	 * keytool -genkeypair -alias fireflySSLkeys -keyalg RSA -keystore fireflySSLkeys.jks
-	 * 
-	 * List credentials in key store
-	 * keytool -list -keystore fireflySSLkeys.jks
-	 * @throws InterruptedException 
-	 */
-	public static void start() throws InterruptedException {
-		ServerBootstrap bootstrap = new ServerBootstrap("firefly-server2.xml");
-		bootstrap.start();
-		
+    public static void main(String[] args) throws Throwable {
+        ServerBootstrap bootstrap = new ServerBootstrap("firefly-server2.xml");
+        bootstrap.start();
+
 //		Thread.sleep(2000L);
 //		bootstrap.stop();
-	}
+    }
 
 }
