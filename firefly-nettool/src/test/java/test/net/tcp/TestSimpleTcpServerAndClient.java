f69d5b47b0ece081010468b76cec8b38a047e03c
hypercube1024 <qptkk@163.com>
2017-02-10 16:34:06 +0800

    add test case of TCP and HTTP

--- firefly-nettool/src/test/java/test/net/tcp/TestSimpleTcpServerAndClient.java
+++ firefly-nettool/src/test/java/test/net/tcp/TestSimpleTcpServerAndClient.java
@@ -2,11 +2,20 @@ package test.net.tcp;
 
 import com.firefly.net.tcp.SimpleTcpClient;
 import com.firefly.net.tcp.SimpleTcpServer;
+import com.firefly.net.tcp.TcpConfiguration;
+import com.firefly.net.tcp.TcpServerConfiguration;
 import com.firefly.net.tcp.codec.StringParser;
 import com.firefly.utils.io.IO;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.Phaser;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -15,13 +24,56 @@ import static org.hamcrest.Matchers.is;
 /**
  * @author Pengtao Qiu
  */
+@RunWith(Parameterized.class)
 public class TestSimpleTcpServerAndClient {
+
+    @Parameter
+    public Run r;
+
+    static class Run {
+        TcpConfiguration clientConfig;
+        TcpServerConfiguration serverConfig;
+        int port;
+        int maxMsg;
+        String testName;
+
+        @Override
+        public String toString() {
+            return testName;
+        }
+    }
+
+    @Parameters(name = "{0}")
+    public static Collection<Run> data() {
+        List<Run> data = new ArrayList<>();
+        Run run = new Run();
+        run.clientConfig = new TcpConfiguration();
+        run.serverConfig = new TcpServerConfiguration();
+        run.port = 1212;
+        run.maxMsg = 5;
+        run.testName = "Test TCP server and client";
+        data.add(run);
+
+        run = new Run();
+        run.clientConfig = new TcpConfiguration();
+        run.clientConfig.setSecureConnectionEnabled(true); // enable TLS
+        run.serverConfig = new TcpServerConfiguration();
+        run.serverConfig.setSecureConnectionEnabled(true);
+        run.port = 1213;
+        run.maxMsg = 20;
+        run.testName = "Test TCP server and client with TLS";
+        data.add(run);
+
+        return data;
+    }
+
 
     @Test
     public void test() {
-        SimpleTcpClient client = new SimpleTcpClient();
-        SimpleTcpServer server = new SimpleTcpServer();
-        int maxMsg = 10;
+        SimpleTcpClient client = new SimpleTcpClient(r.clientConfig);
+        SimpleTcpServer server = new SimpleTcpServer(r.serverConfig);
+        int port = r.port;
+        int maxMsg = r.maxMsg;
         Phaser phaser = new Phaser(3);
 
         server.accept(connection -> {
@@ -44,10 +96,10 @@ public class TestSimpleTcpServerAndClient {
                 }
             });
             connection.receive(parser::receive);
-        }).listen("localhost", 1212);
+        }).listen("localhost", port);
 
 
-        client.connect("localhost", 1212)
+        client.connect("localhost", port)
               .thenAccept(c -> {
                   StringParser parser = new StringParser();
                   AtomicInteger msgCount = new AtomicInteger();
