a042cbff6fef0cf8dd2bd05995ee44f930d16f39
hypercube1024 <qptkk@163.com>
2017-02-16 11:31:56 +0800

    add abstract session handler

---
+++ firefly/src/main/java/com/firefly/server/http2/router/handler/session/AbstractSessionHandler.java
@@ -0,0 +1,42 @@
+package com.firefly.server.http2.router.handler.session;
+
+import com.firefly.server.http2.router.Handler;
+import com.firefly.server.http2.router.RoutingContext;
+import com.firefly.server.http2.router.spi.HTTPSessionHandlerSPI;
+import com.firefly.utils.concurrent.Scheduler;
+import com.firefly.utils.concurrent.Schedulers;
+import com.firefly.utils.lang.AbstractLifeCycle;
+
+/**
+ * @author Pengtao Qiu
+ */
+abstract public class AbstractSessionHandler extends AbstractLifeCycle implements Handler {
+
+    protected final SessionStore sessionStore;
+    protected final HTTPSessionConfiguration configuration;
+    protected final Scheduler scheduler;
+
+    public AbstractSessionHandler(HTTPSessionConfiguration configuration) {
+        this.configuration = configuration;
+        this.sessionStore = createSessionStore();
+        this.scheduler = Schedulers.createScheduler(configuration.getSchedulerPoolSize());
+        start();
+    }
+
+    abstract public SessionStore createSessionStore();
+
+    @Override
+    public void handle(RoutingContext ctx) {
+        HTTPSessionHandlerSPI sessionHandlerSPI = new HTTPSessionHandlerSPIImpl(sessionStore, ctx, scheduler, configuration);
+        ctx.setHTTPSessionHandlerSPI(sessionHandlerSPI);
+    }
+
+    @Override
+    protected void init() {
+    }
+
+    @Override
+    protected void destroy() {
+        scheduler.stop();
+    }
+}
--- firefly/src/main/java/com/firefly/server/http2/router/handler/session/LocalHTTPSessionHandler.java
+++ firefly/src/main/java/com/firefly/server/http2/router/handler/session/LocalHTTPSessionHandler.java
@@ -1,39 +1,17 @@
 package com.firefly.server.http2.router.handler.session;
 
-import com.firefly.server.http2.router.Handler;
-import com.firefly.server.http2.router.RoutingContext;
-import com.firefly.utils.concurrent.Scheduler;
-import com.firefly.utils.concurrent.Schedulers;
-import com.firefly.utils.lang.AbstractLifeCycle;
-
 /**
  * @author Pengtao Qiu
  */
-public class LocalHTTPSessionHandler extends AbstractLifeCycle implements Handler {
-
-    private final SessionStore sessionStore;
-    private final HTTPSessionConfiguration configuration;
-    private final Scheduler scheduler;
+public class LocalHTTPSessionHandler extends AbstractSessionHandler {
 
     public LocalHTTPSessionHandler(HTTPSessionConfiguration configuration) {
-        this.configuration = configuration;
-        this.sessionStore = new LocalSessionStore();
-        this.scheduler = Schedulers.createScheduler(configuration.getSchedulerPoolSize());
-        start();
+        super(configuration);
     }
 
     @Override
-    public void handle(RoutingContext ctx) {
-        HTTPSessionHandlerSPIImpl sessionHandlerSPI = new HTTPSessionHandlerSPIImpl(sessionStore, ctx, scheduler, configuration);
-        ctx.setHTTPSessionHandlerSPI(sessionHandlerSPI);
+    public SessionStore createSessionStore() {
+        return new LocalSessionStore();
     }
 
-    @Override
-    protected void init() {
-    }
-
-    @Override
-    protected void destroy() {
-        scheduler.stop();
-    }
 }
