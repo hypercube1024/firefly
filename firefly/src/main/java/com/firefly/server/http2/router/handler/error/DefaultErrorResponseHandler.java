d5a4bd683c92a800097198e41e8701abfa7b0ddf
hypercube1024 <qptkk@163.com>
2017-02-18 13:21:40 +0800

    add abstract error response handler

--- firefly/src/main/java/com/firefly/server/http2/router/handler/error/DefaultErrorResponseHandler.java
+++ firefly/src/main/java/com/firefly/server/http2/router/handler/error/DefaultErrorResponseHandler.java
@@ -3,37 +3,31 @@ package com.firefly.server.http2.router.handler.error;
 import com.firefly.Version;
 import com.firefly.codec.http2.model.HttpHeader;
 import com.firefly.codec.http2.model.HttpStatus;
-import com.firefly.server.http2.router.Handler;
 import com.firefly.server.http2.router.RoutingContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Pengtao Qiu
  */
-public class DefaultErrorResponseHandler implements Handler {
+public class DefaultErrorResponseHandler extends AbstractErrorResponseHandler {
 
-    private static final Logger log = LoggerFactory.getLogger("firefly-system");
-
-    @Override
-    public void handle(RoutingContext ctx) {
-        if (ctx.hasNext()) {
-            try {
-                ctx.next();
-            } catch (Throwable t) {
-                log.error("http handler exception", t);
-                String content = "The server internal error. <br/>" + t.getMessage();
-                render(ctx, HttpStatus.INTERNAL_SERVER_ERROR_500, content);
-            }
-        } else {
-            String content = "The resource " + ctx.getURI().getPath() + " is not found";
-            render(ctx, HttpStatus.NOT_FOUND_404, content);
-        }
-    }
-
-    public static void render(RoutingContext ctx, int status, String content) {
+    public void render(RoutingContext ctx, int status, Throwable t) {
         HttpStatus.Code code = HttpStatus.getCode(status);
         String title = status + " " + (code != null ? code.getMessage() : "error");
+        String content;
+        switch (code) {
+            case NOT_FOUND: {
+                content = "The resource " + ctx.getURI().getPath() + " is not found";
+            }
+            break;
+            case INTERNAL_SERVER_ERROR: {
+                content = "The server internal error. <br/>" + (t != null ? t.getMessage() : "");
+            }
+            break;
+            default: {
+                content = title + "<br/>" + (t != null ? t.getMessage() : "");
+            }
+            break;
+        }
         ctx.setStatus(status).put(HttpHeader.CONTENT_TYPE, "text/html")
            .write("<!DOCTYPE html>")
            .write("<html>")
