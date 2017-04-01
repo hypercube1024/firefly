e4b15ee1560ba7cd671d94bd8848032b6a7213bc
Alvin Qiu <qptkk@163.com>
2017-04-02 00:50:30 +0800

    [test] : ci build

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -2,6 +2,7 @@
 set -e # halt script on error
 
 rm -f /home/travis/.rvm/gems/ruby-2.3.3/gems/faraday-0.12.0/faraday-0.12.0.gem
+bundle config disable_checksum_validation true
 bundle install
 bundle exec jekyll build
 #bundle exec htmlproofer ./_site
