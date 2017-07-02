845eb2c714919567dc27d9efdaddbcf1b49122b4
Alvin Qiu <qptkk@163.com>
2017-07-02 19:50:26 +0800

    [feat]: add application context extension

---
+++ firefly-kotlin-ext/src/test/kotlin/com/firefly/kotlin/ext/context/FuckService.kt
@@ -0,0 +1,11 @@
+package com.firefly.kotlin.ext.context
+
+import com.firefly.annotation.Component
+
+/**
+ * @author Pengtao Qiu
+ */
+@Component
+class FuckService {
+    fun fuck() = "fuck!~ aHa"
+}
