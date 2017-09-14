ca2bee500a115f978189ad54918e9e532f526015
Alvin Qiu <qptkk@163.com>
2017-09-14 22:55:23 +0800

    [test]: test reactive adapter

--- firefly-example/src/main/java/com/firefly/example/reactive/coffee/store/AppMain.java
+++ firefly-example/src/main/java/com/firefly/example/reactive/coffee/store/AppMain.java
@@ -1,10 +1,23 @@
 package com.firefly.example.reactive.coffee.store;
 
+import com.firefly.$;
+import com.firefly.server.http2.HTTP2ServerBuilder;
+
+import java.util.Collections;
+
 /**
  * @author Pengtao Qiu
  */
 public class AppMain {
+
+    private static final String root = "template/coffeeStore";
+
     public static void main(String[] args) {
-        System.out.println("hello coffee store");
+        HTTP2ServerBuilder s = $.httpServer();
+        s.router().get("/").asyncHandler(ctx -> {
+            ctx.renderTemplate(root + "/index.mustache", Collections.emptyList());
+        });
+
+        s.listen("localhost", 8080);
     }
 }
