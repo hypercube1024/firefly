6954fac03430305de3caa563c56423bc6b846e4e
Alvin Qiu <qptkk@163.com>
2017-09-17 21:54:33 +0800

    [test]: add bootstrap css framework

--- firefly-example/src/main/java/com/firefly/example/reactive/coffee/store/router/impl/sys/SysRouterInstaller.java
+++ firefly-example/src/main/java/com/firefly/example/reactive/coffee/store/router/impl/sys/SysRouterInstaller.java
@@ -4,12 +4,20 @@ import com.firefly.annotation.Component;
 import com.firefly.annotation.Inject;
 import com.firefly.example.reactive.coffee.store.router.RouterInstaller;
 import com.firefly.server.http2.HTTP2ServerBuilder;
+import com.firefly.server.http2.router.handler.file.StaticFileHandler;
+import com.firefly.utils.log.slf4j.ext.LazyLogger;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * @author Pengtao Qiu
  */
 @Component("sysRouterInstaller")
 public class SysRouterInstaller implements RouterInstaller {
+
+    private static final LazyLogger logger = LazyLogger.create();
 
     @Inject
     private HTTP2ServerBuilder server;
@@ -27,6 +35,14 @@ public class SysRouterInstaller implements RouterInstaller {
 
         // HTTP transaction manager
         server.router().methods(transactionalHandler.getMethods()).handler(transactionalHandler);
+
+        // static file
+        try {
+            Path path = Paths.get(SysRouterInstaller.class.getResource("/").toURI());
+            server.router().get("/static/*").handler(new StaticFileHandler(path.toAbsolutePath().toString()));
+        } catch (URISyntaxException e) {
+            logger.error(() -> "setup static file router exception", e);
+        }
     }
 
     @Override
