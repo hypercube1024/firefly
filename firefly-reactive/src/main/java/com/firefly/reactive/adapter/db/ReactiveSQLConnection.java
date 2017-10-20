211ae34c4455e04acc95eb5d0c3baf395b12d670
Alvin Qiu <qptkk@163.com>
2017-10-20 21:19:49 +0800

    [doc]: add document for ReactiveSQLConnection

--- firefly-reactive/src/main/java/com/firefly/reactive/adapter/db/ReactiveSQLConnection.java
+++ firefly-reactive/src/main/java/com/firefly/reactive/adapter/db/ReactiveSQLConnection.java
@@ -1,5 +1,6 @@
 package com.firefly.reactive.adapter.db;
 
+import com.firefly.db.RecordNotFound;
 import com.firefly.db.SQLConnection;
 import com.firefly.db.SQLResultSet;
 import com.firefly.db.TransactionIsolation;
@@ -10,21 +11,68 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * SQLConnection reactor adapter
+ * SQLConnection reactor adapter. It wraps SQLConnection using Spring reactor.
  *
  * @see com.firefly.db.SQLConnection
  * @author Pengtao Qiu
  */
 public interface ReactiveSQLConnection {
 
+    /**
+     * Query single column record by SQL
+     *
+     * @param sql    An SQL that may contain one or more '?' IN parameter placeholders
+     * @param params SQL parameters
+     * @param <T>    The type of column
+     * @return The future result
+     * @throws RecordNotFound If the database has not record throw RecordNotFound exception
+     */
     <T> Mono<T> queryForSingleColumn(String sql, Object... params);
 
+    /**
+     * Query record and bind object
+     *
+     * @param sql    An SQL that may contain one or more '?' IN parameter placeholders
+     * @param clazz  The Class reference of bound object
+     * @param params SQL parameters
+     * @param <T>    The type of bound object
+     * @return The future result
+     * @throws RecordNotFound If the database has not record throw RecordNotFound exception
+     */
     <T> Mono<T> queryForObject(String sql, Class<T> clazz, Object... params);
 
+    /**
+     * Query record by id
+     *
+     * @param id    Primary key
+     * @param clazz The Class reference of bound object
+     * @param <T>   The type of bound object
+     * @return The future result
+     * @throws RecordNotFound If the database has not record of this id throw RecordNotFound exception
+     */
     <T> Mono<T> queryById(Object id, Class<T> clazz);
 
+    /**
+     * Query records and convert records to a Map
+     *
+     * @param sql        An SQL that may contain one or more '?' IN parameter placeholders
+     * @param valueClass The Class reference of bound object
+     * @param params     SQL parameters
+     * @param <K>        The type of primary key
+     * @param <V>        The type of bound object
+     * @return The future result that contains a map, the key is primary key of record, the value is bound object.
+     */
     <K, V> Mono<Map<K, V>> queryForBeanMap(String sql, Class<V> valueClass, Object... params);
 
+    /**
+     * Query records and bind object
+     *
+     * @param sql    An SQL that may contain one or more '?' IN parameter placeholders
+     * @param clazz  The Class reference of bound object
+     * @param params SQL parameters
+     * @param <T>    The type of bound object
+     * @return The future result that contains a list
+     */
     <T> Mono<List<T>> queryForList(String sql, Class<T> clazz, Object... params);
 
     <T> Mono<T> query(String sql, Func1<SQLResultSet, T> handler, Object... params);
