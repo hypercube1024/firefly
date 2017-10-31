110c284fc0cf71a70ebc65d5b893a7b71ef21186
hypercube1024 <qptkk@163.com>
2017-10-31 16:32:24 +0800

    [refactor]: move shutdown hook to AbstractLifeCycle

--- firefly-common/src/main/java/com/firefly/utils/lang/AbstractLifeCycle.java
+++ firefly-common/src/main/java/com/firefly/utils/lang/AbstractLifeCycle.java
@@ -1,49 +1,53 @@
 package com.firefly.utils.lang;
 
 public abstract class AbstractLifeCycle implements LifeCycle {
-	
-	protected volatile boolean start;
 
-	@Override
-	public boolean isStarted() {
-		return start;
-	}
+    protected volatile boolean start;
 
-	@Override
-	public boolean isStopped() {
-		return !start;
-	}
-	
-	@Override
-	public void start() {
-		if (isStarted())
-			return;
+    public AbstractLifeCycle() {
+        Runtime.getRuntime().addShutdownHook(new Thread(this::stop, "the firefly shutdown thread"));
+    }
 
-		synchronized (this) {
-			if (isStarted())
-				return;
-			
-			init();
-			start = true;
-		}
-	}
+    @Override
+    public boolean isStarted() {
+        return start;
+    }
 
-	@Override
-	public void stop() {
-		if (isStopped())
-			return;
+    @Override
+    public boolean isStopped() {
+        return !start;
+    }
 
-		synchronized (this) {
-			if (isStopped())
-				return;
-			
-			destroy();
-			start = false;
-		}
-	}
-	
-	abstract protected void init();
-	
-	abstract protected void destroy();
+    @Override
+    public void start() {
+        if (isStarted())
+            return;
+
+        synchronized (this) {
+            if (isStarted())
+                return;
+
+            init();
+            start = true;
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (isStopped())
+            return;
+
+        synchronized (this) {
+            if (isStopped())
+                return;
+
+            destroy();
+            start = false;
+        }
+    }
+
+    abstract protected void init();
+
+    abstract protected void destroy();
 
 }
