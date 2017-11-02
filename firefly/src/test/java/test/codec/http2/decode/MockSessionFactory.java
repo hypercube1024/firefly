ae2e995b4c38748482b90fd9283d6f5befef65e7
hypercube1024 <qptkk@163.com>
2017-11-02 14:12:34 +0800

    [test]: mock Session using Mockito

---
+++ firefly/src/test/java/test/codec/http2/decode/MockSessionFactory.java
@@ -0,0 +1,96 @@
+package test.codec.http2.decode;
+
+import com.firefly.net.ByteBufferArrayOutputEntry;
+import com.firefly.net.OutputEntry;
+import com.firefly.net.Session;
+import com.firefly.utils.concurrent.Callback;
+import com.firefly.utils.io.BufferUtils;
+
+import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Pengtao Qiu
+ */
+public class MockSessionFactory {
+
+    public final LinkedList<ByteBuffer> output = new LinkedList<>();
+
+    public Session create() {
+        return mock(AbstractMockSession.class, withSettings().useConstructor(output).defaultAnswer(CALLS_REAL_METHODS));
+    }
+
+    abstract public static class AbstractMockSession implements Session {
+        public Object attachment;
+        public boolean isOpen = true;
+        public final LinkedList<ByteBuffer> outboundData;
+
+        public AbstractMockSession(LinkedList<ByteBuffer> outboundData) {
+            this.outboundData = outboundData;
+        }
+
+        @Override
+        public void attachObject(Object attachment) {
+            this.attachment = attachment;
+        }
+
+        @Override
+        public Object getAttachment() {
+            return attachment;
+        }
+
+        @Override
+        public void encode(Object message) {
+            if (message instanceof ByteBufferArrayOutputEntry) {
+                ByteBufferArrayOutputEntry outputEntry = (ByteBufferArrayOutputEntry) message;
+                write(outputEntry);
+            }
+        }
+
+        @Override
+        public Session.State getState() {
+            return State.OPEN;
+        }
+
+        @Override
+        public boolean isOpen() {
+            return isOpen;
+        }
+
+        @Override
+        public void write(ByteBuffer byteBuffer, Callback callback) {
+            outboundData.offer(byteBuffer);
+            byteBuffer.flip();
+            callback.succeeded();
+        }
+
+        @Override
+        public void write(ByteBuffer[] buffers, Callback callback) {
+            for (ByteBuffer buffer : buffers) {
+                outboundData.offer(buffer);
+                buffer.flip();
+            }
+            callback.succeeded();
+
+        }
+
+        @Override
+        public void write(Collection<ByteBuffer> buffers, Callback callback) {
+            write(buffers.toArray(BufferUtils.EMPTY_BYTE_BUFFER_ARRAY), callback);
+        }
+
+        @Override
+        public void write(OutputEntry<?> entry) {
+            ByteBufferArrayOutputEntry outputEntry = (ByteBufferArrayOutputEntry) entry;
+            write(outputEntry.getData(), outputEntry.getCallback());
+        }
+
+        @Override
+        public void closeNow() {
+            isOpen = false;
+        }
+    }
+}
