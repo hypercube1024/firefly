209ca005bba1ad84ac51be2241fe629c45b073af
hypercube1024 <qptkk@163.com>
2017-12-01 11:46:28 +0800

    [refactor]: merge http1 output buffers to a buffer array

--- firefly/src/main/java/com/firefly/server/http2/ServerSecureEncoder.java
+++ firefly/src/main/java/com/firefly/server/http2/ServerSecureEncoder.java
@@ -14,15 +14,16 @@ public class ServerSecureEncoder extends EncoderChain {
         HTTPConnection connection = (HTTPConnection) session.getAttachment();
 
         switch (connection.getHttpVersion()) {
-            case HTTP_2: {
+            case HTTP_2:
                 HTTP2ServerConnection http2ServerConnection = (HTTP2ServerConnection) connection;
                 http2ServerConnection.writeEncryptMessage(message);
-            }
-            break;
+                break;
             case HTTP_1_1:
+                HTTP1ServerConnection http1ServerConnection = (HTTP1ServerConnection) connection;
                 if (message instanceof ByteBuffer) {
-                    HTTP1ServerConnection http1ServerConnection = (HTTP1ServerConnection) connection;
                     http1ServerConnection.getSecureSession().write((ByteBuffer) message, Callback.NOOP);
+                } else if (message instanceof ByteBuffer[]) {
+                    http1ServerConnection.getSecureSession().write((ByteBuffer[]) message, Callback.NOOP);
                 } else {
                     throw new IllegalArgumentException(
                             "the http1 encoder must receive the ByteBuffer, but this message type is "
