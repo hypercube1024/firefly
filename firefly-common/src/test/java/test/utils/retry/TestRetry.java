95c4518ea702b38d4fa8130a0c1c1f41c232b76c
hypercube1024 <qptkk@163.com>
2017-12-12 13:28:25 +0800

    [test]: test retry api

--- firefly-common/src/test/java/test/utils/retry/TestRetry.java
+++ firefly-common/src/test/java/test/utils/retry/TestRetry.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import static com.firefly.utils.function.Predicates.of;
 import static com.firefly.utils.retry.RetryStrategies.ifException;
@@ -23,7 +22,7 @@ public class TestRetry {
     @Test
     public void test() {
         boolean success = RetryTaskBuilder.<Boolean>newTask()
-                .retry(ifException(of(ex -> ex instanceof IOException).or(ex -> ex instanceof TimeoutException)))
+                .retry(ifException(of(ex -> ex instanceof IOException).or(ex -> ex instanceof RuntimeException)))
                 .retry(ifResult(isEqual(false)))
                 .stop(afterAttempt(3))
                 .wait(fixedWait(1, TimeUnit.SECONDS))
