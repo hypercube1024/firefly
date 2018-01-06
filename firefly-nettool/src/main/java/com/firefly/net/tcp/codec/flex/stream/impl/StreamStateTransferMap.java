64b4db76726b3e745668faf5f1ce6d823a8e4bce
Alvin Qiu <qptkk@163.com>
2018-01-06 23:39:50 +0800

    [refactor]: optimize StreamStateTransferMap

--- firefly-nettool/src/main/java/com/firefly/net/tcp/codec/flex/stream/impl/StreamStateTransferMap.java
+++ firefly-nettool/src/main/java/com/firefly/net/tcp/codec/flex/stream/impl/StreamStateTransferMap.java
@@ -2,9 +2,9 @@ package com.firefly.net.tcp.codec.flex.stream.impl;
 
 import com.firefly.net.tcp.codec.flex.stream.Stream;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 import static com.firefly.net.tcp.codec.flex.stream.impl.StreamStateTransferMap.Op.RECV_ES;
 import static com.firefly.net.tcp.codec.flex.stream.impl.StreamStateTransferMap.Op.SEND_ES;
@@ -14,40 +14,40 @@ import static com.firefly.net.tcp.codec.flex.stream.impl.StreamStateTransferMap.
  */
 public class StreamStateTransferMap {
 
-    private static final List<Node> rules = new ArrayList<>();
+    private static final Map<Node, Stream.State> rules = new HashMap<>();
 
     static {
         Node newStream = new Node();
         newStream.op = SEND_ES;
         newStream.from = Stream.State.OPEN;
-        newStream.to = Stream.State.LOCALLY_CLOSED;
-        rules.add(newStream);
+        rules.put(newStream, Stream.State.LOCALLY_CLOSED);
 
         newStream = new Node();
         newStream.op = RECV_ES;
         newStream.from = Stream.State.OPEN;
-        newStream.to = Stream.State.REMOTELY_CLOSED;
-        rules.add(newStream);
+        rules.put(newStream, Stream.State.REMOTELY_CLOSED);
 
         Node locallyClosedStream = new Node();
         locallyClosedStream.op = RECV_ES;
         locallyClosedStream.from = Stream.State.LOCALLY_CLOSED;
-        locallyClosedStream.to = Stream.State.CLOSED;
-        rules.add(locallyClosedStream);
+        rules.put(locallyClosedStream, Stream.State.CLOSED);
 
         Node remotelyClosed = new Node();
         remotelyClosed.op = SEND_ES;
         remotelyClosed.from = Stream.State.REMOTELY_CLOSED;
-        remotelyClosed.to = Stream.State.CLOSED;
-        rules.add(remotelyClosed);
+        rules.put(remotelyClosed, Stream.State.CLOSED);
     }
 
     public static Stream.State getNextState(Stream.State currentState, Op op) {
-        return rules.stream()
-                    .filter(node -> node.op == op && node.from == currentState)
-                    .map(node -> node.to)
-                    .findFirst()
-                    .orElseThrow(() -> new IllegalStateException("The stream state error. " + currentState + ", " + op));
+        Node node = new Node();
+        node.from = currentState;
+        node.op = op;
+        Stream.State state = rules.get(node);
+        if (state == null) {
+            throw new IllegalStateException("The stream state error. " + currentState + ", " + op);
+        } else {
+            return state;
+        }
     }
 
     public enum Op {
@@ -57,7 +57,19 @@ public class StreamStateTransferMap {
     private static class Node {
         Op op;
         Stream.State from;
-        Stream.State to;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node = (Node) o;
+            return op == node.op && from == node.from;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(op, from);
+        }
     }
 
 
