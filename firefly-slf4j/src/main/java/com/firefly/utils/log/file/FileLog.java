18ac1b2cb3bc5edbd18b0e3c0e1d7f20055285fe
hypercube1024 <qptkk@163.com>
2018-02-09 10:20:48 +0800

    [feat]: add log filter

--- firefly-slf4j/src/main/java/com/firefly/utils/log/file/FileLog.java
+++ firefly-slf4j/src/main/java/com/firefly/utils/log/file/FileLog.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.Objects;
+import java.util.Optional;
 
 public class FileLog implements Log, Closeable {
 
@@ -35,6 +36,8 @@ public class FileLog implements Log, Closeable {
     private LogOutputStream output = new LogOutputStream();
 
     void write(LogItem logItem) {
+        Optional.ofNullable(logFilter).ifPresent(f -> f.filter(logItem));
+
         if (consoleOutput) {
             System.out.println(logFormatter.format(logItem));
         }
