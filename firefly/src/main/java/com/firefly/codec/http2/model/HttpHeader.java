55dc75ee6bcb0ec7accf08f1294d7e18acbac502
hypercube1024 <qptkk@163.com>
2018-08-01 11:40:22 +0800

    [feat]: add CORS headers

--- firefly/src/main/java/com/firefly/codec/http2/model/HttpHeader.java
+++ firefly/src/main/java/com/firefly/codec/http2/model/HttpHeader.java
@@ -115,9 +115,20 @@ public enum HttpHeader {
     C_PATH(":path"),
     C_STATUS(":status"),
 
+    /**
+     * CORS Fields
+     */
+    ACCESS_CONTROL_ALLOW_ORIGIN("Access-Control-Allow-Origin"),
+    ACCESS_CONTROL_EXPOSE_HEADERS("Access-Control-Expose-Headers"),
+    ACCESS_CONTROL_ALLOW_CREDENTIALS("Access-Control-Allow-Credentials"),
+    ACCESS_CONTROL_ALLOW_METHODS("Access-Control-Allow-Methods"),
+    ACCESS_CONTROL_ALLOW_HEADERS("Access-Control-Allow-Headers"),
+    ACCESS_CONTROL_REQUEST_METHOD("Access-Control-Request-Method"),
+    ACCESS_CONTROL_REQUEST_HEADERS("Access-Control-Request-Headers"),
+
     UNKNOWN("::UNKNOWN::");
 
-    public final static Trie<HttpHeader> CACHE = new ArrayTrie<>(630);
+    public final static Trie<HttpHeader> CACHE = new ArrayTrie<>(900);
 
     static {
         for (HttpHeader header : HttpHeader.values())
