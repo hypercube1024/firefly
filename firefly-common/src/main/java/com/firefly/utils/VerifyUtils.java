584f08d6179156b6f68d0f61e2b96b3e6fac898b
hypercube1024 <qptkk@163.com>
2018-10-01 00:06:40 +0800

    [style]: clean up codes

--- firefly-common/src/main/java/com/firefly/utils/VerifyUtils.java
+++ firefly-common/src/main/java/com/firefly/utils/VerifyUtils.java
@@ -3,8 +3,9 @@ package com.firefly.utils;
 abstract public class VerifyUtils {
 
     public static boolean isNumeric(String str) {
-        if (isEmpty(str))
+        if (isEmpty(str)) {
             return false;
+        }
 
         char first = str.charAt(0);
         int i = first == '-' ? 1 : 0;
@@ -17,8 +18,9 @@ abstract public class VerifyUtils {
     }
 
     public static boolean isInteger(String str) {
-        if (isEmpty(str))
+        if (isEmpty(str)) {
             return false;
+        }
 
         char first = str.charAt(0);
         int i = first == '-' ? 1 : 0;
@@ -28,13 +30,14 @@ abstract public class VerifyUtils {
             }
         }
 
-        Long t = Long.parseLong(str);
+        long t = Long.parseLong(str);
         return t <= Integer.MAX_VALUE && t >= Integer.MIN_VALUE;
     }
 
     public static boolean isLong(String str) {
-        if (isEmpty(str))
+        if (isEmpty(str)) {
             return false;
+        }
 
         char first = str.charAt(0);
         char end = str.charAt(str.length() - 1);
@@ -56,12 +59,14 @@ abstract public class VerifyUtils {
     }
 
     public static boolean isFloat(String str) {
-        if (isEmpty(str))
+        if (isEmpty(str)) {
             return false;
+        }
 
         char end = str.charAt(str.length() - 1);
-        if (!(end == 'f' || end == 'F'))
+        if (!(end == 'f' || end == 'F')) {
             return false;
+        }
 
         int point = 0;
         int i = str.charAt(0) == '-' ? 1 : 0;
@@ -77,8 +82,9 @@ abstract public class VerifyUtils {
     }
 
     public static boolean isDouble(String str) {
-        if (isEmpty(str))
+        if (isEmpty(str)) {
             return false;
+        }
 
         int point = 0;
         int i = str.charAt(0) == '-' ? 1 : 0;
@@ -98,27 +104,11 @@ abstract public class VerifyUtils {
         return ch >= '0' && ch <= '9';
     }
 
-    public static boolean isNotEmpty(Long o) {
-        return o != null && StringUtils.hasText(o.toString());
+    public static boolean isNotEmpty(String str) {
+        return StringUtils.hasText(str);
     }
 
-    public static boolean isNotEmpty(Integer o) {
-        return o != null && StringUtils.hasText(o.toString());
-    }
-
-    public static boolean isNotEmpty(String o) {
-        return StringUtils.hasText(o);
-    }
-
-    public static boolean isEmpty(Long o) {
-        return !isNotEmpty(o);
-    }
-
-    public static boolean isEmpty(Integer o) {
-        return !isNotEmpty(o);
-    }
-
-    public static boolean isEmpty(String o) {
-        return !isNotEmpty(o);
+    public static boolean isEmpty(String str) {
+        return !isNotEmpty(str);
     }
 }
