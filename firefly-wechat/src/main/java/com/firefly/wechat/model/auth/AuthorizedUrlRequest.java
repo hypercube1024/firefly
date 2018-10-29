b7c1cac633061c65dfebc493617a76fbe56b0e7a
hypercube1024 <qptkk@163.com>
2018-10-29 17:31:48 +0800

    [feat]: add buildAuthorizedUrl method

---
+++ firefly-wechat/src/main/java/com/firefly/wechat/model/auth/AuthorizedUrlRequest.java
@@ -0,0 +1,51 @@
+package com.firefly.wechat.model.auth;
+
+import java.io.Serializable;
+
+/**
+ * @author Pengtao Qiu
+ */
+public class AuthorizedUrlRequest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public static final String SCOPE_BASE = "snsapi_base";
+    public static final String SCOPE_USERINFO = "snsapi_userinfo";
+
+    private String appid;
+    private String redirectUri;
+    private String scope; // snsapi_base or snsapi_userinfo
+    private String state;
+
+    public String getAppid() {
+        return appid;
+    }
+
+    public void setAppid(String appid) {
+        this.appid = appid;
+    }
+
+    public String getRedirectUri() {
+        return redirectUri;
+    }
+
+    public void setRedirectUri(String redirectUri) {
+        this.redirectUri = redirectUri;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
