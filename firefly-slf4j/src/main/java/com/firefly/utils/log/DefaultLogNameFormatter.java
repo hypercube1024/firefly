4ef172479ec003f2021c757615a393d92bbab75a
hypercube1024 <qptkk@163.com>
2018-11-14 10:16:12 +0800

    [feat]: update default log format

--- firefly-slf4j/src/main/java/com/firefly/utils/log/DefaultLogNameFormatter.java
+++ firefly-slf4j/src/main/java/com/firefly/utils/log/DefaultLogNameFormatter.java
@@ -1,13 +1,30 @@
 package com.firefly.utils.log;
 
-import com.firefly.utils.time.TimeUtils;
-
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
+import static java.time.temporal.ChronoField.*;
 
 /**
  * @author Pengtao Qiu
  */
 public class DefaultLogNameFormatter implements LogNameFormatter {
+
+    public static final DateTimeFormatter DEFAULT_LOG_NAME_FORMATTER = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .append(ISO_LOCAL_DATE)
+            .appendLiteral('_')
+            .append(new DateTimeFormatterBuilder()
+                    .appendValue(HOUR_OF_DAY, 2)
+                    .appendLiteral('-')
+                    .appendValue(MINUTE_OF_HOUR, 2)
+                    .optionalStart()
+                    .appendLiteral('-')
+                    .appendValue(SECOND_OF_MINUTE, 2)
+                    .toFormatter())
+            .toFormatter();
 
     @Override
     public String format(String name, LocalDateTime localDateTime) {
@@ -16,6 +33,6 @@ public class DefaultLogNameFormatter implements LogNameFormatter {
 
     @Override
     public String formatBak(String name, LocalDateTime localDateTime, int index) {
-        return name + "." + localDateTime.format(TimeUtils.FILE_NAME_LOCAL_DATE_TIME) + "." + index + ".bak.txt";
+        return name + "." + localDateTime.format(DEFAULT_LOG_NAME_FORMATTER) + "." + index + ".bak.txt";
     }
 }
