ec53226dee7cd695b753d35c95c71b9f919430cf
Alvin Qiu <qptkk@163.com>
2018-12-02 14:09:37 +0800

    [feat]: add coroutine pool queue size limit

--- firefly-common/src/main/kotlin/com/fireflysource/common/coroutine/CommonCoroutinePool.kt
+++ firefly-common/src/main/kotlin/com/fireflysource/common/coroutine/CommonCoroutinePool.kt
@@ -2,7 +2,10 @@ package com.fireflysource.common.coroutine
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.asCoroutineDispatcher
-import java.util.concurrent.*
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.ForkJoinPool
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
 
 /**
@@ -13,6 +16,14 @@ object CoroutineDispatchers {
         "com.fireflysource.common.coroutine.defaultPoolSize",
         Runtime.getRuntime().availableProcessors()
                                                  )
+    val ioBlockingQueueSize = Integer.getInteger(
+        "com.fireflysource.common.coroutine.ioBlockingQueueSize",
+        20000
+                                                )
+    val singleThreadQueueSize = Integer.getInteger(
+        "com.fireflysource.common.coroutine.singleThreadQueueSize",
+        20000
+                                                  )
 
     val computation: CoroutineDispatcher by lazy {
         ForkJoinPool(defaultPoolSize, { pool ->
@@ -26,17 +37,19 @@ object CoroutineDispatchers {
         ThreadPoolExecutor(
             defaultPoolSize, 64,
             30L, TimeUnit.SECONDS,
-            ArrayBlockingQueue<Runnable>(20000)
+            ArrayBlockingQueue<Runnable>(ioBlockingQueueSize)
                           ) { r ->
             Thread(r, "firefly-io-blocking-pool-" + threadId.getAndIncrement())
         }.asCoroutineDispatcher()
     }
     val singleThread: CoroutineDispatcher by lazy {
-        Executors.newSingleThreadExecutor { r ->
-            Thread(
-                r,
-                "firefly-single-thread-pool"
-                  )
+        val threadId = AtomicInteger()
+        ThreadPoolExecutor(
+            1, 1,
+            0, TimeUnit.MILLISECONDS,
+            ArrayBlockingQueue<Runnable>(singleThreadQueueSize)
+                          ) { r ->
+            Thread(r, "firefly-single-thread-pool-" + threadId.getAndIncrement())
         }.asCoroutineDispatcher()
     }
 }
