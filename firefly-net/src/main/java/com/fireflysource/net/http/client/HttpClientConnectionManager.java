4fdd177f7b1325e4e94a1c09bfcf136359fa048b
pengtao.qiu <qptkk@163.com>
2018-12-27 22:35:50 +0800

    [feat]: add http client connection manager API

--- firefly-net/src/main/java/com/fireflysource/net/http/client/HttpClientConnectionManager.java
+++ firefly-net/src/main/java/com/fireflysource/net/http/client/HttpClientConnectionManager.java
@@ -1,9 +1,23 @@
 package com.fireflysource.net.http.client;
 
+import java.net.SocketAddress;
+import java.util.concurrent.CompletableFuture;
+
 /**
- *
+ * The HTTP client connection manager maintains the HTTP client connections. If it creates connection and negotiates the
+ * HTTP version is 2.0, and it maintains only one connection to send the request and receive the response.
+ * If the HTTP version is 1.1, it maintains the  HTTP connections in a pool.
  *
  * @author Pengtao Qiu
  */
 public interface HttpClientConnectionManager {
+
+    /**
+     * Get HTTP client connection.
+     *
+     * @param address The socket address.
+     * @return The HTTP client connection.
+     */
+    CompletableFuture<HttpClientConnection> getConnection(SocketAddress address);
+
 }
