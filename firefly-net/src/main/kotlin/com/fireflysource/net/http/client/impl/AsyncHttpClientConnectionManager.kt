6619d4f5ea008a9584392aaf813c4822eb7818a7
Alvin Qiu <qptkk@163.com>
2019-02-17 15:15:25 +0800

    [test]: implement HttpClientConnectionManager

--- firefly-net/src/main/kotlin/com/fireflysource/net/http/client/impl/AsyncHttpClientConnectionManager.kt
+++ firefly-net/src/main/kotlin/com/fireflysource/net/http/client/impl/AsyncHttpClientConnectionManager.kt
@@ -1,11 +1,14 @@
 package com.fireflysource.net.http.client.impl
 
+import com.fireflysource.common.coroutine.asyncGlobally
 import com.fireflysource.net.http.client.HttpClientConnection
 import com.fireflysource.net.http.client.HttpClientConnectionManager
 import com.fireflysource.net.http.client.HttpClientRequest
 import com.fireflysource.net.tcp.TcpClient
 import com.fireflysource.net.tcp.aio.AioTcpClient
 import com.fireflysource.net.tcp.secure.SecureEngineFactory
+import kotlinx.coroutines.future.asCompletableFuture
+import kotlinx.coroutines.future.await
 import java.util.concurrent.CompletableFuture
 
 class AsyncHttpClientConnectionManager(
@@ -25,7 +28,19 @@ class AsyncHttpClientConnectionManager(
             .enableSecureConnection()
     }
 
-    override fun getConnection(request: HttpClientRequest): CompletableFuture<HttpClientConnection> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getConnection(request: HttpClientRequest): CompletableFuture<HttpClientConnection> = asyncGlobally {
+        when (request.uri.scheme) {
+            "http" -> {
+                // TODO
+                val tcpConnection = tcpClient.connect(request.uri.host, request.uri.port).await()
+                Http1ClientConnection(tcpConnection)
+            }
+            "https" -> {
+                // TODO
+                val tcpConnection = secureTcpClient.connect(request.uri.host, request.uri.port).await()
+                Http1ClientConnection(tcpConnection)
+            }
+            else -> throw IllegalArgumentException("not support the protocol: ${request.uri.scheme}")
+        }
+    }.asCompletableFuture()
 }
