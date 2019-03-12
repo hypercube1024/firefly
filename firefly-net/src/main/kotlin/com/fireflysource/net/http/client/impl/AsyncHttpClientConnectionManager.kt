ccd475001c7b513b33b9e627e849a0463fec705e
pengtao.qiu <qptkk@163.com>
2019-03-12 13:18:04 +0800

    [test]: test

--- firefly-net/src/main/kotlin/com/fireflysource/net/http/client/impl/AsyncHttpClientConnectionManager.kt
+++ firefly-net/src/main/kotlin/com/fireflysource/net/http/client/impl/AsyncHttpClientConnectionManager.kt
@@ -1,15 +1,20 @@
 package com.fireflysource.net.http.client.impl
 
 import com.fireflysource.common.coroutine.asyncGlobally
+import com.fireflysource.common.pool.Pool
 import com.fireflysource.net.http.client.HttpClientConnection
 import com.fireflysource.net.http.client.HttpClientConnectionManager
 import com.fireflysource.net.http.client.HttpClientRequest
+import com.fireflysource.net.http.client.HttpClientResponse
+import com.fireflysource.net.http.common.model.HttpVersion
 import com.fireflysource.net.tcp.TcpClient
 import com.fireflysource.net.tcp.aio.AioTcpClient
 import com.fireflysource.net.tcp.secure.SecureEngineFactory
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.future.asCompletableFuture
 import kotlinx.coroutines.future.await
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ConcurrentHashMap
 
 class AsyncHttpClientConnectionManager(
     timeout: Long,
@@ -28,6 +33,12 @@ class AsyncHttpClientConnectionManager(
             .enableSecureConnection()
     }
 
+    private val reqHostPortHttpVersionMap: Map<ReqHostPort, HttpVersion> = ConcurrentHashMap()
+    private val unknowVersionReqChannel = Channel<RequestEvent>(Channel.UNLIMITED)
+    private val reqHostPortHttp1ConnPoolMap: Map<ReqHostPort, Pool<HttpClientConnection>> = ConcurrentHashMap()
+    private val reqHostHttp2ConnMap: Map<ReqHostPort, HttpClientConnection> = ConcurrentHashMap()
+
+
     override fun getConnection(request: HttpClientRequest): CompletableFuture<HttpClientConnection> = asyncGlobally {
         when (request.uri.scheme) {
             "http" -> {
@@ -44,3 +55,7 @@ class AsyncHttpClientConnectionManager(
         }
     }.asCompletableFuture()
 }
+
+data class ReqHostPort(val host: String, val port: Int)
+
+data class RequestEvent(val request: HttpClientRequest, val responseFuture: CompletableFuture<HttpClientResponse>)
