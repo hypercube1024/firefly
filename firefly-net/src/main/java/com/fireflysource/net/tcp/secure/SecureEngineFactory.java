3cebab851880a67fdfe9fa5eee3e9194eca2b895
pengtao.qiu <qptkk@163.com>
2020-02-06 18:14:05 +0800

    [feat]: add default coroutine scope APIs for secure engine factory.

--- firefly-net/src/main/java/com/fireflysource/net/tcp/secure/SecureEngineFactory.java
+++ firefly-net/src/main/java/com/fireflysource/net/tcp/secure/SecureEngineFactory.java
@@ -33,7 +33,31 @@ public interface SecureEngineFactory {
      * @param supportedProtocols The supported application layer protocols.
      * @return The TLS engine.
      */
-    SecureEngine create(CoroutineScope coroutineScope, boolean clientMode, String peerHost, int peerPort,
-                        List<String> supportedProtocols);
+    SecureEngine create(CoroutineScope coroutineScope, boolean clientMode, String peerHost, int peerPort, List<String> supportedProtocols);
 
+    /**
+     * Create a TLS engine by default coroutine scope.
+     *
+     * @param clientMode         If true, the current connection is the client tcp connection.
+     * @param supportedProtocols The supported application layer protocols.
+     * @return The TLS engine.
+     */
+    default SecureEngine create(boolean clientMode, List<String> supportedProtocols) {
+        return create(null, clientMode, supportedProtocols);
+    }
+
+    /**
+     * Create a TLS engine by default coroutine scope and advisory peer information.
+     * Applications using this factory method are providing hints for an internal session reuse strategy.
+     * Some cipher suites (such as Kerberos) require remote hostname information, in which case peerHost needs to be specified.
+     *
+     * @param clientMode         If true, the current connection is the client tcp connection.
+     * @param peerHost           the non-authoritative name of the host.
+     * @param peerPort           the non-authoritative port.
+     * @param supportedProtocols The supported application layer protocols.
+     * @return The TLS engine.
+     */
+    default SecureEngine create(boolean clientMode, String peerHost, int peerPort, List<String> supportedProtocols) {
+        return create(null, clientMode, peerHost, peerPort, supportedProtocols);
+    }
 }
