432db96937c43fe63c02a0e895f749ee7b538cc6
Alvin Qiu <qptkk@163.com>
2020-02-09 16:17:02 +0800

    [fix]: catch client connect exception.

--- firefly-net/src/main/kotlin/com/fireflysource/net/tcp/aio/AioTcpClient.kt
+++ firefly-net/src/main/kotlin/com/fireflysource/net/tcp/aio/AioTcpClient.kt
@@ -5,6 +5,7 @@ import com.fireflysource.net.tcp.TcpClient
 import com.fireflysource.net.tcp.TcpConnection
 import com.fireflysource.net.tcp.secure.SecureEngineFactory
 import com.fireflysource.net.tcp.secure.conscrypt.NoCheckConscryptSSLContextFactory
+import kotlinx.coroutines.CoroutineScope
 import java.net.SocketAddress
 import java.net.StandardSocketOptions
 import java.nio.channels.AsynchronousSocketChannel
@@ -15,7 +16,7 @@ import java.util.concurrent.CompletableFuture
 /**
  * @author Pengtao Qiu
  */
-class AioTcpClient(val config: TcpConfig = TcpConfig()) : AbstractAioTcpChannelGroup(), TcpClient {
+class AioTcpClient(private val config: TcpConfig = TcpConfig()) : AbstractAioTcpChannelGroup(), TcpClient {
 
     companion object {
         private val log = SystemLogger.create(AioTcpClient::class.java)
@@ -55,6 +56,38 @@ class AioTcpClient(val config: TcpConfig = TcpConfig()) : AbstractAioTcpChannelG
         supportedProtocols: List<String>
     ): CompletableFuture<TcpConnection> {
         val future = CompletableFuture<TcpConnection>()
+        try {
+            connect(address, peerHost, peerPort, supportedProtocols, future)
+        } catch (e: Exception) {
+            log.warn(e) { "connecting exception. $address" }
+            future.completeExceptionally(e)
+        }
+        return future
+    }
+
+    private fun connect(
+        address: SocketAddress,
+        peerHost: String,
+        peerPort: Int,
+        supportedProtocols: List<String>,
+        future: CompletableFuture<TcpConnection>
+    ) {
+
+        fun createSecureEngine(scope: CoroutineScope) = if (peerHost.isNotBlank() && peerPort != 0) {
+            secureEngineFactory.create(scope, true, peerHost, peerPort, supportedProtocols)
+        } else {
+            secureEngineFactory.create(scope, true, supportedProtocols)
+        }
+
+        fun createSecureConnection(connectionId: Int, socketChannel: AsynchronousSocketChannel) =
+            if (config.enableSecureConnection) {
+                val tcpConnection =
+                    AioTcpConnection(connectionId, config.timeout, socketChannel, getDispatcher(connectionId))
+                val secureEngine = createSecureEngine(tcpConnection.coroutineScope)
+                AioSecureTcpConnection(tcpConnection, secureEngine)
+            } else {
+                AioTcpConnection(connectionId, config.timeout, socketChannel, getDispatcher(connectionId))
+            }
 
         val socketChannel = AsynchronousSocketChannel.open(group)
         socketChannel.setOption(StandardSocketOptions.SO_REUSEADDR, config.reuseAddr)
@@ -64,38 +97,18 @@ class AioTcpClient(val config: TcpConfig = TcpConfig()) : AbstractAioTcpChannelG
 
             override fun completed(result: Void?, connectionId: Int) {
                 try {
-                    val tcpConnection = if (config.enableSecureConnection) {
-                        val tcpConnection = AioTcpConnection(
-                            connectionId, config.timeout,
-                            socketChannel, getDispatcher(connectionId)
-                        )
-                        val secureEngine = if (peerHost.isNotBlank() && peerPort != 0) {
-                            secureEngineFactory.create(
-                                tcpConnection.coroutineScope,
-                                true,
-                                peerHost,
-                                peerPort,
-                                supportedProtocols
-                            )
-                        } else {
-                            secureEngineFactory.create(tcpConnection.coroutineScope, true, supportedProtocols)
-                        }
-                        AioSecureTcpConnection(tcpConnection, secureEngine)
-                    } else {
-                        AioTcpConnection(connectionId, config.timeout, socketChannel, getDispatcher(connectionId))
-                    }
-                    future.complete(tcpConnection)
+                    future.complete(createSecureConnection(connectionId, socketChannel))
                 } catch (e: Exception) {
-                    log.warn(e) { "connecting exception. $connectionId" }
+                    log.warn(e) { "connecting exception. id: ${connectionId}, address: $address" }
+                    future.completeExceptionally(e)
                 }
             }
 
             override fun failed(t: Throwable?, connectionId: Int) {
-                log.warn(t) { "connecting exception. $connectionId" }
+                log.warn(t) { "connecting exception. id: ${connectionId}, address: $address" }
                 future.completeExceptionally(t)
             }
         })
-        return future
     }
 
     override fun getThreadName() = "aio-tcp-client"
