c37429c948ad9e84b886f3d23d1227217f1881ff
pengtao.qiu <qptkk@163.com>
2020-02-13 14:13:50 +0800

    [test]: trailer test case.

--- firefly-net/src/test/kotlin/com/fireflysource/net/http/client/impl/TestHttp2ClientConnection.kt
+++ firefly-net/src/test/kotlin/com/fireflysource/net/http/client/impl/TestHttp2ClientConnection.kt
@@ -22,6 +22,7 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import java.util.*
@@ -194,6 +195,130 @@ class TestHttp2ClientConnection {
             val response = futures[0].await()
             assertEquals(HttpStatus.OK_200, response.status)
             assertEquals("receive data success.", response.stringBody)
+        }
+
+        val throughput = count / (time / 1000.00)
+        println("success. $time ms, ${throughput.roundToLong()} qps")
+    }
+
+    @Test
+    @DisplayName("should receive trailers successfully.")
+    fun testTrailer(): Unit = runBlocking {
+        val host = "localhost"
+        val port = Random.nextInt(20000, 30000)
+        val tcpConfig = TcpConfig(30, true)
+        val httpConfig = HttpConfig()
+        val count = 100
+
+        AioTcpServer(tcpConfig).onAcceptAsync { connection ->
+            connection.startReadingAndAwaitHandshake()
+            val connectionListener = object : Http2Connection.Listener.Adapter() {
+
+                override fun onFailure(http2Connection: Http2Connection, failure: Throwable) {
+                    failure.printStackTrace()
+                }
+
+                override fun onClose(http2Connection: Http2Connection, frame: GoAwayFrame) {
+                    println("Server receives go away frame: $frame")
+                }
+
+                override fun onNewStream(stream: Stream, frame: HeadersFrame): Stream.Listener {
+                    val headers = LinkedList<HeadersFrame>()
+                    var responseClient100Continue = false
+                    val trailer = HttpFields()
+                    var receivedData = false
+
+                    fun response100Continue(stream: Stream) {
+                        if (!responseClient100Continue) {
+                            val has100Continue = headers
+                                .map { it.metaData.fields }
+                                .any { HttpHeaderValue.CONTINUE.`is`(it[HttpHeader.EXPECT]) }
+                            if (has100Continue) {
+                                val continueResponse =
+                                    MetaData.Response(HttpVersion.HTTP_2, HttpStatus.CONTINUE_100, HttpFields())
+                                val continueHeaders = HeadersFrame(stream.id, continueResponse, null, false)
+                                stream.headers(continueHeaders) {}
+                                responseClient100Continue = true
+                            }
+                        }
+                    }
+
+                    fun onMessageComplete(stream: Stream) {
+                        val fields = HttpFields()
+                        fields.put("Test-100-Continue", "100")
+                        fields.addCSV(HttpHeader.TRAILER, "t1", "t2", "t3")
+                        val response = MetaData.Response(HttpVersion.HTTP_2, HttpStatus.OK_200, fields)
+                        val headersFrame = HeadersFrame(stream.id, response, null, false)
+                        stream.headers(headersFrame) {}
+
+                        val data = BufferUtils.toBuffer("receive data success.")
+                        val dataFrame = DataFrame(stream.id, data, false)
+                        stream.data(dataFrame) {}
+
+                        val trailers = HttpFields()
+                        trailers.put("t1", "v1")
+                        trailers.put("t2", "v2")
+                        trailers.put("t3", "v3")
+                        val trailersFrame = HeadersFrame(stream.id, MetaData(HttpVersion.HTTP_2, trailers), null, true)
+                        stream.headers(trailersFrame) {}
+                    }
+
+                    headers.add(frame)
+                    response100Continue(stream)
+                    if (frame.isEndStream) {
+                        onMessageComplete(stream)
+                    }
+                    return object : Stream.Listener.Adapter() {
+
+                        override fun onHeaders(stream: Stream, headersFrame: HeadersFrame) {
+
+                            if (receivedData) {
+                                trailer.addAll(headersFrame.metaData.fields)
+                            } else {
+                                headers.add(headersFrame)
+                            }
+                            response100Continue(stream)
+                            if (headersFrame.isEndStream) {
+                                onMessageComplete(stream)
+                            }
+                        }
+
+                        override fun onData(stream: Stream, dataFrame: DataFrame, result: Consumer<Result<Void>>) {
+                            receivedData = true
+                            response100Continue(stream)
+                            if (dataFrame.isEndStream) {
+                                onMessageComplete(stream)
+                            }
+                            result.accept(Result.SUCCESS)
+                        }
+                    }
+                }
+            }
+            Http2ServerConnection(httpConfig, connection, SimpleFlowControlStrategy(), connectionListener)
+        }.listen(host, port)
+
+        val httpClient = HttpClientFactory.create()
+
+        val time = measureTimeMillis {
+            val futures = (1..count).map {
+                httpClient.post("https://${host}:${port}/data")
+                    .put(HttpHeader.EXPECT, HttpHeaderValue.CONTINUE.value)
+                    .body("Some test data!")
+                    .submit()
+            }
+            CompletableFuture.allOf(*futures.toTypedArray()).await()
+            val allDone = futures.all { it.isDone }
+            assertTrue(allDone)
+
+            val response = futures[0].await()
+            assertEquals(HttpStatus.OK_200, response.status)
+            assertEquals("receive data success.", response.stringBody)
+            assertTrue(response.trailerSupplier != null)
+            val trailers = response.trailerSupplier.get()
+            println(trailers)
+            assertEquals("v1", trailers["t1"])
+            assertEquals("v2", trailers["t2"])
+            assertEquals("v3", trailers["t3"])
         }
 
         val throughput = count / (time / 1000.00)
