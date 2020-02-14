d53b11ed7c20904d6d17940a4e4b8d524893b0c2
pengtao.qiu <qptkk@163.com>
2020-02-14 18:00:57 +0800

    [feat]: HttpServerContentHandler implements AsyncCloseable interface

--- firefly-net/src/main/kotlin/com/fireflysource/net/http/server/impl/content/handler/ByteBufferContentHandler.kt
+++ firefly-net/src/main/kotlin/com/fireflysource/net/http/server/impl/content/handler/ByteBufferContentHandler.kt
@@ -3,9 +3,18 @@ package com.fireflysource.net.http.server.impl.content.handler
 import com.fireflysource.net.http.server.HttpServerContentHandler
 import com.fireflysource.net.http.server.RoutingContext
 import java.nio.ByteBuffer
+import java.util.concurrent.CompletableFuture
 
 class ByteBufferContentHandler : HttpServerContentHandler {
     override fun accept(byteBuffer: ByteBuffer, context: RoutingContext) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun closeFuture(): CompletableFuture<Void> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun close() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
