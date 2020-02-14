0b857365d3fe967f37a18b9e82893ea3bea394bb
pengtao.qiu <qptkk@163.com>
2020-02-14 17:59:32 +0800

    [feat]: HttpServerContentHandler implements AsyncCloseable interface

--- firefly-net/src/main/java/com/fireflysource/net/http/server/HttpServerContentHandler.java
+++ firefly-net/src/main/java/com/fireflysource/net/http/server/HttpServerContentHandler.java
@@ -1,8 +1,9 @@
 package com.fireflysource.net.http.server;
 
+import com.fireflysource.common.io.AsyncCloseable;
+
 import java.nio.ByteBuffer;
 import java.util.function.BiConsumer;
 
-@FunctionalInterface
-public interface HttpServerContentHandler extends BiConsumer<ByteBuffer, RoutingContext> {
+public interface HttpServerContentHandler extends BiConsumer<ByteBuffer, RoutingContext>, AsyncCloseable {
 }
