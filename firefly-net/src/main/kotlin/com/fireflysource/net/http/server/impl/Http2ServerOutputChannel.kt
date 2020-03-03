3a8bc8d1db1e81b9563cb4815699f41af1da2ece
pengtao.qiu <qptkk@163.com>
2020-03-03 11:10:47 +0800

    [refactor]: merge byte buffer array

--- firefly-net/src/main/kotlin/com/fireflysource/net/http/server/impl/Http2ServerOutputChannel.kt
+++ firefly-net/src/main/kotlin/com/fireflysource/net/http/server/impl/Http2ServerOutputChannel.kt
@@ -1,8 +1,11 @@
 package com.fireflysource.net.http.server.impl
 
 import com.fireflysource.common.io.BufferUtils
+import com.fireflysource.common.io.flipToFill
+import com.fireflysource.common.io.flipToFlush
 import com.fireflysource.common.sys.Result
 import com.fireflysource.common.sys.Result.discard
+import com.fireflysource.common.sys.SystemLogger
 import com.fireflysource.net.http.common.model.MetaData
 import com.fireflysource.net.http.common.v2.frame.DataFrame
 import com.fireflysource.net.http.common.v2.frame.HeadersFrame
@@ -24,6 +27,11 @@ class Http2ServerOutputChannel(
     private val response: MetaData.Response,
     private val stream: Stream
 ) : HttpServerOutputChannel {
+
+    companion object {
+        private val log = SystemLogger.create(Http2ServerOutputChannel::class.java)
+        private val bufferArrayChunkSize = 16 * 1024 * 1024L
+    }
 
     private val committed = AtomicBoolean(false)
     private val closed = AtomicBoolean(false)
@@ -92,32 +100,59 @@ class Http2ServerOutputChannel(
         closeFuture()
     }
 
-    private fun writeOutputMessage(): Boolean {
+    private fun writeOutputMessage() {
         val message = messages.poll()
-        return if (message != null) {
+        if (message != null) {
             val last = messages.isEmpty() && response.trailerSupplier == null
             when (message) {
-                is HeadersOutputMessage -> {
-                    val headersFrame = HeadersFrame(stream.id, response, null, last)
-                    stream.headers(headersFrame, discard())
-                }
-                is BufferOutputMessage -> {
-                    val dataFrame = DataFrame(stream.id, message.byteBuffer, last)
-                    stream.data(dataFrame, discard())
-                }
-                is BuffersOutputMessage -> {
-                    val offset = message.getCurrentOffset()
-                    val lastIndex = message.getLastIndex()
-                    (offset..lastIndex).forEach {
-                        val endStream = last && (it == lastIndex)
-                        val byteBuffer = message.byteBuffers[it]
-                        val dataFrame = DataFrame(stream.id, byteBuffer, endStream)
-                        stream.data(dataFrame, discard())
+                is HeadersOutputMessage -> writeHeaders(last)
+                is BufferOutputMessage -> writeBuffer(message, last)
+                is BuffersOutputMessage -> writeBuffers(message, last)
+            }
+        }
+    }
+
+    private fun writeHeaders(last: Boolean) {
+        val headersFrame = HeadersFrame(stream.id, response, null, last)
+        stream.headers(headersFrame, discard())
+    }
+
+    private fun writeBuffer(message: BufferOutputMessage, last: Boolean) {
+        val dataFrame = DataFrame(stream.id, message.byteBuffer, last)
+        stream.data(dataFrame, discard())
+    }
+
+    private fun writeBuffers(message: BuffersOutputMessage, last: Boolean) {
+        val length = message.getCurrentLength()
+        when {
+            length == 1 -> {
+                val byteBuffer = message.byteBuffers[message.getCurrentOffset()]
+                val dataFrame = DataFrame(stream.id, byteBuffer, last)
+                stream.data(dataFrame, discard())
+            }
+            length > 1 -> {
+                while (message.hasRemaining()) {
+                    val remaining = message.remaining()
+                    val size = remaining.coerceAtMost(bufferArrayChunkSize).toInt()
+                    val buffer = BufferUtils.allocate(size)
+                    val pos = buffer.flipToFill()
+
+                    log.debug { "HTTP2 outputs buffer array. before remaining: $remaining, size: $size" }
+                    while (buffer.hasRemaining()) {
+                        val offset = message.getCurrentOffset()
+                        val src = message.byteBuffers[offset]
+                        BufferUtils.put(src, buffer)
+                        log.debug { "HTTP2 outputs buffer array. put offset: $offset" }
                     }
+
+                    buffer.flipToFlush(pos)
+                    val end = last && !message.hasRemaining()
+                    val dataFrame = DataFrame(stream.id, buffer, end)
+                    stream.data(dataFrame, discard())
+                    log.debug { "HTTP2 outputs buffer array. after remaining: ${message.remaining()}, end: $end" }
                 }
             }
-            true
-        } else false
+        }
     }
 
 }
