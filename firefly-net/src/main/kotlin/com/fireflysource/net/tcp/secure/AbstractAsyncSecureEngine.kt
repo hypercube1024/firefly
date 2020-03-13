15edbc3409cb99b4ca2aea50600c78df0992f082
pengtao.qiu <qptkk@163.com>
2020-03-13 09:33:28 +0800

    [feat]: modify app buffer size.

--- firefly-net/src/main/kotlin/com/fireflysource/net/tcp/secure/AbstractAsyncSecureEngine.kt
+++ firefly-net/src/main/kotlin/com/fireflysource/net/tcp/secure/AbstractAsyncSecureEngine.kt
@@ -39,7 +39,7 @@ abstract class AbstractAsyncSecureEngine(
     private var writeFunction: Function<ByteBuffer, CompletableFuture<Int>>? = null
 
     private var inPacketBuffer = EMPTY_BUFFER
-    private val inAppBuffer = BufferUtils.allocateDirect(sslEngine.session.applicationBufferSize * 2)
+    private val inAppBuffer = BufferUtils.allocateDirect(sslEngine.session.applicationBufferSize)
     private val outPacketBuffer = BufferUtils.allocateDirect(sslEngine.session.packetBufferSize)
 
     private val closed = AtomicBoolean(false)
