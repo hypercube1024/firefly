542bd259b3715a4ad63dfb409c8b208a80463728
pengtao.qiu <qptkk@163.com>
2020-04-08 15:27:56 +0800

    [feat]: add http method matcher.

---
+++ firefly-net/src/main/kotlin/com/fireflysource/net/http/server/impl/matcher/HttpMethodMatcher.kt
@@ -0,0 +1,20 @@
+package com.fireflysource.net.http.server.impl.matcher
+
+import com.fireflysource.net.http.server.Matcher
+import com.fireflysource.net.http.server.Router
+
+class HttpMethodMatcher : AbstractPreciseMatcher() {
+
+    override fun add(rule: String, router: Router) {
+        super.add(rule.toUpperCase(), router)
+    }
+
+    override fun match(value: String): Matcher.MatchResult? {
+        return super.match(value.toUpperCase())
+    }
+
+    override fun getMatchType(): Matcher.MatchType {
+        return Matcher.MatchType.METHOD
+    }
+
+}
---
+++ firefly-net/src/test/kotlin/com/fireflysource/net/http/server/impl/matcher/TestHttpMethodMatcher.kt
@@ -0,0 +1,42 @@
+package com.fireflysource.net.http.server.impl.matcher
+
+import com.fireflysource.net.http.common.model.HttpMethod
+import com.fireflysource.net.http.server.Matcher
+import com.fireflysource.net.http.server.impl.AsyncRouter
+import com.fireflysource.net.http.server.impl.AsyncRouterManager
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+
+class TestHttpMethodMatcher {
+
+    private val routerManager = Mockito.mock(AsyncRouterManager::class.java)
+
+    @Test
+    @DisplayName("should match router by the http method successfully.")
+    fun test() {
+        val matcher = HttpMethodMatcher()
+        val router1 = AsyncRouter(1, routerManager)
+        val router2 = AsyncRouter(2, routerManager)
+        val router3 = AsyncRouter(3, routerManager)
+        matcher.add(HttpMethod.GET.value, router1)
+        matcher.add("get", router2)
+        matcher.add("POST", router3)
+
+        val result1 = matcher.match("GET")
+        requireNotNull(result1)
+        assertEquals(2, result1.routers.size)
+        assertEquals(Matcher.MatchType.METHOD, result1.matchType)
+
+        val result2 = matcher.match("get")
+        requireNotNull(result2)
+        assertEquals(2, result2.routers.size)
+        assertEquals(Matcher.MatchType.METHOD, result2.matchType)
+
+        val result3 = matcher.match("POST")
+        requireNotNull(result3)
+        assertEquals(1, result3.routers.size)
+        assertEquals(Matcher.MatchType.METHOD, result3.matchType)
+    }
+}
