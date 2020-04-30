6b2d0f2b895950c7afbbd42c210f5dcff98d0238
pengtao.qiu <qptkk@163.com>
2020-04-30 16:59:46 +0800

    [test]: test performance

--- firefly-net/src/test/kotlin/com/fireflysource/net/http/server/impl/HttpServerDemo.kt
+++ firefly-net/src/test/kotlin/com/fireflysource/net/http/server/impl/HttpServerDemo.kt
@@ -8,6 +8,22 @@ import com.fireflysource.net.http.server.impl.router.handler.FileHandler
 import java.nio.file.Paths
 import java.util.*
 
+/*
+wrk -t6 -c12 -d60s --latency http://localhost:9999/test
+Running 1m test @ http://localhost:9999/test
+6 threads and 12 connections
+Thread Stats   Avg      Stdev     Max   +/- Stdev
+Latency   140.75us   41.13us   2.74ms   97.43%
+Req/Sec    13.92k   837.61    17.39k    75.68%
+Latency Distribution
+50%  137.00us
+75%  144.00us
+90%  153.00us
+99%  247.00us
+4993095 requests in 1.00m, 419.04MB read
+Requests/sec:  83080.51
+Transfer/sec:      6.97MB
+*/
 fun main() {
     val httpServer = HttpServerFactory.create()
     val corsConfig = CorsConfig("*")
