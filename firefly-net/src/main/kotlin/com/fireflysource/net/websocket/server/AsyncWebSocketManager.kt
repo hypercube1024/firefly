73a3412787ac74926fef1802a93fe778919dc12e
Alvin Qiu <qptkk@163.com>
2020-06-10 12:38:41 +0800

    [feat]: implement websocket manager and connection builder.

--- firefly-net/src/main/kotlin/com/fireflysource/net/websocket/server/AsyncWebSocketManager.kt
+++ firefly-net/src/main/kotlin/com/fireflysource/net/websocket/server/AsyncWebSocketManager.kt
@@ -1,15 +1,26 @@
 package com.fireflysource.net.websocket.server
 
+import com.fireflysource.common.`object`.Assert
+
 /**
  * @author Pengtao Qiu
  */
 class AsyncWebSocketManager : WebSocketManager {
 
-    override fun register(connectionHandler: WebSocketServerConnectionHandler?) {
-        TODO("Not yet implemented")
+    private val webSocketHandlers: MutableMap<String, WebSocketServerConnectionHandler> = HashMap()
+
+    override fun register(connectionHandler: WebSocketServerConnectionHandler) {
+        Assert.notNull(connectionHandler.url, "The websocket url must be not null")
+        Assert.notNull(connectionHandler.extensionSelector, "The websocket extension selector must be not null")
+        Assert.notNull(connectionHandler.subProtocolSelector, "The websocket sub protocol selector must be not null")
+        Assert.notNull(connectionHandler.policy, "The websocket policy must be not null")
+        Assert.notNull(connectionHandler.connectionListener, "The websocket connection listener must be not null")
+        Assert.notNull(connectionHandler.messageHandler, "The websocket message handler must be not null")
+
+        webSocketHandlers[connectionHandler.url] = connectionHandler
     }
 
-    override fun findWebSocketHandler(path: String?): WebSocketServerConnectionHandler {
-        TODO("Not yet implemented")
+    override fun findWebSocketHandler(path: String): WebSocketServerConnectionHandler? {
+        return webSocketHandlers[path]
     }
 }
